fb4c9ac2dc520652f55bb64542eb03a82a3326f5
umeneses <umeneses@student.42sp.org.br>
2024-06-12 15:32:51 -0300

    1st version of minishell main

--- src/minishell_main.c
+++ src/minishell_main.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 10:53:12 by umeneses          #+#    #+#             */
-/*   Updated: 2024/06/12 10:53:14 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/06/12 15:11:41 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,29 @@
 int	main(void)
 {
 	char	*input;
+	char	**cmd;
+	char	*path;
+	pid_t	pid;
 
+	cmd = NULL;
+	path = NULL;
+	ft_printf("Welcome to Minishell\n");
 	while (1)
 	{
 		input = readline(GREEN"Minishell: "RESET);
-		printf("%s\n", input);
+		if (ft_strncmp(input, "exit", 4) == 0)
+			break ;
+		pid = fork();
+		if (!pid)
+		{
+			cmd = ft_split(input, ' ');
+			path = ft_strjoin("/usr/bin/", cmd[0]);
+			execve(path, cmd, __environ);
+			printf(RED"Error: %s\n"RESET, strerror(errno));
+			exit(0);
+		}
+		else
+			waitpid(pid, NULL, 0);
 		free (input);
 	}
 	return (0);
