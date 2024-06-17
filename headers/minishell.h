bb1e4c948d56a77f6a4fb402d17dda736ff71e6b
umeneses <umeneses@student.42sp.org.br>
2024-06-17 13:27:54 -0300

    adding first structs and token enums

--- headers/minishell.h
+++ headers/minishell.h
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 10:53:20 by umeneses          #+#    #+#             */
-/*   Updated: 2024/06/12 14:15:14 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/06/17 11:32:53 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,32 @@
 # define GREEN   "\x1b[32m"
 # define BLUE    "\x1b[34m"
 # define RESET   "\x1b[0m"
+
+typedef struct s_token
+{
+	int		type;
+	char	*lexeme;
+}			t_token;
+
+typedef struct s_token_list
+{
+	t_token				*token;
+	struct s_token_list	*prev;
+	struct s_token_list	*next;
+}						t_token_list;
+
+enum e_token
+{
+	WORD,
+	PIPE,
+	REDIRECT_INPUT,
+	REDIRECT_HEREDOC,
+	REDIRECT_OUTPUT,
+	REDIRECT_OUTPUT_APPEND,
+	OR,
+	AND,
+	OPEN_PARENTHESIS,
+	CLOSE_PARENTHESIS
+};
 
 #endif
