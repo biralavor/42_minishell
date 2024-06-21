de6de50890f62efa9eece66a5290b8bb13751fb0
umeneses <umeneses@student.42sp.org.br>
2024-06-21 18:38:39 -0300

    adding new input tester variation

--- src/simple_main.c
+++ src/simple_main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   simple_main.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tmalheir <tmalheir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 10:53:12 by umeneses          #+#    #+#             */
-/*   Updated: 2024/06/21 14:58:10 by tmalheir         ###   ########.fr       */
+/*   Updated: 2024/06/21 18:33:47 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int	main(void)
 {
 	char	*input;
 
-	input = "Test";
-//	input = "&&(This) < < | > || \"is\" \'a\' test&&";
+	// input = "&&Test";
+	input = "&&(This) < < | > || \"is\" \'a\' test&&";
 	printf("%s\n", input);
 	check_userinput(input);
 	return (0);
