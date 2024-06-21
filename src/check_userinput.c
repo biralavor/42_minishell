d13d92623183996b4adba5258d940086edd0f114
Thais Malheiros Assumpcao <tmalheir@c3r4p5.42sp.org.br>
2024-06-21 11:27:34 -0300

    Creating function to receive simple_main's user input and sending it to create_token_list function.

---
+++ src/check_userinput.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_userinput.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmalheir <tmalheir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/19 09:20:45 by tmalheir          #+#    #+#             */
+/*   Updated: 2024/06/21 09:48:00 by tmalheir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	check_userinput(char *str)
+{
+	if (check_initial_errors(str))
+		exit (EXIT_FAILURE);
+	create_token_list(str);
+}
