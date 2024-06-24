a2e2b6948fd62ebfa8017742566f9f707cd571c9
umeneses <umeneses@student.42sp.org.br>
2024-06-24 13:57:56 -0300

    adding WORD  solo tests

--- _ci_cd/test_files/minishell_tester.c
+++ _ci_cd/test_files/minishell_tester.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 11:12:53 by umeneses          #+#    #+#             */
-/*   Updated: 2024/06/24 13:43:08 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/06/24 13:57:42 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -225,6 +225,142 @@ MU_TEST(check_userinput_token_empty)
 	mu_assert_string_eq(xpect_node_01_lexeme, actual_node_01_lexeme);
 	mu_assert_string_eq(xpect_node_01_type, actual_node_01_type);
 }
+
+MU_TEST(check_userinput_token_01_word_with_space)
+{
+	// ARRANGE
+	char			*userinput;
+	int				idx;
+	int				xpect_lst_size;
+	int				actual_lst_size;
+	char			*xpect_node1_lexeme;
+	char			*xpect_node1_type;
+	char			*xpect_node2_lexeme;
+	char			*xpect_node2_type;
+	char			*xpect_node3_lexeme;
+	char			*xpect_node3_type;
+	char			*actual_node1_lexeme;
+	char			*actual_node1_type;
+	char			*actual_node2_lexeme;
+	char			*actual_node2_type;
+	char			*actual_node3_lexeme;
+	char			*actual_node3_type;
+	t_token_list	*lst;
+
+	//ACT
+	userinput = "Mussum Ipsum";
+
+	xpect_node1_type = get_token_string(WORD);
+	xpect_node1_lexeme = "Mussum";
+	xpect_node2_type = get_token_string(WORD);
+	xpect_node2_lexeme = "Ipsum";
+	xpect_node3_type = NULL;
+	xpect_node3_lexeme = NULL;
+	xpect_lst_size = 2;
+	idx = 0;
+	lst = NULL;
+	get_state(idx, userinput, &lst);
+
+	actual_lst_size = ft_lst_size(lst);
+	if (actual_lst_size == 2 && lst->next->next == NULL)
+	{
+		actual_node1_lexeme = lst->lexeme;
+		actual_node1_type = get_token_string(lst->type);
+		lst = lst->next;
+		actual_node2_lexeme = lst->lexeme;
+		actual_node2_type = get_token_string(lst->type);
+		actual_node3_lexeme = NULL;
+		actual_node3_type = NULL;
+	}
+	else
+	{
+		actual_node1_lexeme = lst->lexeme;
+		actual_node1_type = get_token_string(lst->type);
+		lst = lst->next;
+		actual_node2_lexeme = lst->lexeme;
+		actual_node2_type = get_token_string(lst->type);
+		lst = lst->next;
+		actual_node3_lexeme = lst->lexeme;
+		actual_node3_type = get_token_string(lst->type);
+	}
+
+	// ASSERT
+	mu_assert_int_eq(xpect_lst_size, actual_lst_size);
+	mu_assert_string_eq(xpect_node1_lexeme, actual_node1_lexeme);
+	mu_assert_string_eq(xpect_node1_type, actual_node1_type);
+	mu_assert_string_eq(xpect_node2_lexeme, actual_node2_lexeme);
+	mu_assert_string_eq(xpect_node2_type, actual_node2_type);
+	mu_assert_string_eq(xpect_node3_lexeme, actual_node3_lexeme);
+	mu_assert_string_eq(xpect_node3_type, actual_node3_type);
+}
+
+// MU_TEST(check_userinput_token_01_word_with_tab)
+// {
+// 	// ARRANGE
+// 	char			*userinput;
+// 	int				idx;
+// 	int				xpect_lst_size;
+// 	int				actual_lst_size;
+// 	char			*xpect_node1_lexeme;
+// 	char			*xpect_node1_type;
+// 	char			*xpect_node2_lexeme;
+// 	char			*xpect_node2_type;
+// 	char			*xpect_node3_lexeme;
+// 	char			*xpect_node3_type;
+// 	char			*actual_node1_lexeme;
+// 	char			*actual_node1_type;
+// 	char			*actual_node2_lexeme;
+// 	char			*actual_node2_type;
+// 	char			*actual_node3_lexeme;
+// 	char			*actual_node3_type;
+// 	t_token_list	*lst;
+
+// 	//ACT
+// 	userinput = "Mussum	Ipsum";
+
+// 	xpect_node1_type = get_token_string(WORD);
+// 	xpect_node1_lexeme = "Mussum";
+// 	xpect_node2_type = get_token_string(WORD);
+// 	xpect_node2_lexeme = "Ipsum";
+// 	xpect_node3_type = NULL;
+// 	xpect_node3_lexeme = NULL;
+// 	xpect_lst_size = 2;
+// 	idx = 0;
+// 	lst = NULL;
+// 	get_state(idx, userinput, &lst);
+
+// 	actual_lst_size = ft_lst_size(lst);
+// 	if (actual_lst_size == 2 && lst->next->next == NULL)
+// 	{
+// 		actual_node1_lexeme = lst->lexeme;
+// 		actual_node1_type = get_token_string(lst->type);
+// 		lst = lst->next;
+// 		actual_node2_lexeme = lst->lexeme;
+// 		actual_node2_type = get_token_string(lst->type);
+// 		actual_node3_lexeme = NULL;
+// 		actual_node3_type = NULL;
+// 	}
+// 	else
+// 	{
+// 		actual_node1_lexeme = lst->lexeme;
+// 		actual_node1_type = get_token_string(lst->type);
+// 		lst = lst->next;
+// 		actual_node2_lexeme = lst->lexeme;
+// 		actual_node2_type = get_token_string(lst->type);
+// 		lst = lst->next;
+// 		actual_node3_lexeme = lst->lexeme;
+// 		actual_node3_type = get_token_string(lst->type);
+// 	}
+
+// 	// ASSERT
+// 	mu_assert_int_eq(xpect_lst_size, actual_lst_size);
+// 	mu_assert_string_eq(xpect_node1_lexeme, actual_node1_lexeme);
+// 	mu_assert_string_eq(xpect_node1_type, actual_node1_type);
+// 	mu_assert_string_eq(xpect_node2_lexeme, actual_node2_lexeme);
+// 	mu_assert_string_eq(xpect_node2_type, actual_node2_type);
+// 	mu_assert_string_eq(xpect_node3_lexeme, actual_node3_lexeme);
+// 	mu_assert_string_eq(xpect_node3_type, actual_node3_type);
+// }
 
 MU_TEST(check_userinput_token_01_pipe_at_begin)
 {
@@ -1938,6 +2074,8 @@ MU_TEST_SUITE(token_simulation_errors_suite)
 MU_TEST_SUITE(check_userinput_tokens_light_tests)
 {
 	MU_RUN_TEST(check_userinput_token_empty);
+	MU_RUN_TEST(check_userinput_token_01_word_with_space);
+	// MU_RUN_TEST(check_userinput_token_01_word_with_tab);
 	MU_RUN_TEST(check_userinput_token_01_pipe_at_begin);
 	MU_RUN_TEST(check_userinput_token_01_pipe_at_middle);
 	MU_RUN_TEST(check_userinput_token_01_pipe_at_end);
