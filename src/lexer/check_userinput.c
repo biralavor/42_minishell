5cec8078ae35a21b153d6a5ebb4519a07f1a07c1
Thais Malheiros Assumpcao <tmalheir@c1r3p1.42sp.org.br>
2024-06-27 14:54:09 -0300

    bugfix removing index unused

--- src/lexer/check_userinput.c
+++ src/lexer/check_userinput.c
@@ -6,7 +6,7 @@
 /*   By: tmalheir <tmalheir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 09:20:45 by tmalheir          #+#    #+#             */
-/*   Updated: 2024/06/27 14:45:54 by tmalheir         ###   ########.fr       */
+/*   Updated: 2024/06/27 14:52:34 by tmalheir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,8 @@
 
 void	check_userinput(char *str)
 {
-	int				idx;
 	t_token_list	*lst;
 
-	idx = 0;
 	if (!(check_initial_errors(str)))
 		lexer_error();
 	lst = NULL;
