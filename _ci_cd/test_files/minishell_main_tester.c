c2151436f33a64e9a762d69c179196b7d77b16b9
umeneses <umeneses@student.42sp.org.br>
2024-07-05 16:22:30 -0300

    updating renamed file links

--- _ci_cd/test_files/minishell_main_tester.c
+++ _ci_cd/test_files/minishell_main_tester.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 11:12:53 by umeneses          #+#    #+#             */
-/*   Updated: 2024/07/03 12:12:51 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/07/05 16:13:09 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,18 @@
 #include "../../src/lexer/03.check_initial_errors.c"
 #include "../../src/lexer/04.create_token_list.c"
 #include "../../src/lexer/05.get_state.c"
-#include "../../src/lexer/06.state1_to_state5.c"
-#include "../../src/lexer/07.state6_to_state10.c"
-#include "../../src/lexer/08.state11_to_state15.c"
+#include "../../src/lexer/06.state10_to_state30.c"
+#include "../../src/lexer/07.state40_to_state60.c"
+#include "../../src/lexer/08.state61_to_state90.c"
 #include "../../src/lexer/09.error_manager_lexer.c"
 #include "../../src/lexer/10.lexer_utils.c"
-#include "../../src/parser/check_syntax_errors.c"
-#include "../../src/parser/error_manager_parser.c"
-#include "../../src/parser/parser_utils.c"
+#include "../../src/parser/11.syntax_analysis.c"
+#include "../../src/parser/12.state100.c"
+#include "../../src/parser/13.utils_parsing_word.c"
+#include "../../src/parser/14.state200.c"
+#include "../../src/parser/15.state300.c"
+#include "../../src/parser/16.state400.c"
+#include "../../src/parser/17.error_manager_parser.c"
 #include "../_tdd_utils/ft_lst_print_content.c"
 
 #include "minunit_utils.c"
