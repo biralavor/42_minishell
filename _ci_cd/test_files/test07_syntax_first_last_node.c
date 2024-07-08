74aeeab9fcaf987280180469eb83dc5044351cf0
biralavor <umeneses@student.42sp.org.br>
2024-07-08 11:43:19 -0300

    adding redirect input at first node tests

--- _ci_cd/test_files/test07_syntax_first_last_node.c
+++ _ci_cd/test_files/test07_syntax_first_last_node.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 11:01:23 by umeneses          #+#    #+#             */
-/*   Updated: 2024/07/08 11:28:39 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/07/08 11:42:03 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -283,6 +283,99 @@ MU_TEST(testing_syntax_first_node_AND_space_before)
 	mu_assert_int_eq(xpect_lst_size, actual_lst_size);
 	mu_assert_int_eq(xpect_syntax_validation, actual_syntax_validation);
 }
+
+
+MU_TEST(testing_syntax_first_node_REDIRECT_INPUT_simple_space)
+{
+	// ARRANGE
+	char			*userinput;
+	int				idx;
+	int				xpect_lst_size;
+	int				actual_lst_size;
+	int				xpect_syntax_validation;
+	int				actual_syntax_validation;
+	int				fd;
+	t_token_list	*lst;
+
+	//ACT
+	fd = 0;
+	open_redirect_stderr_to_dev_null(fd);
+	userinput = "> Mussum|Ipsum";
+	xpect_syntax_validation = 301; // begining with redirect input
+	xpect_lst_size = 4;
+	idx = 0;
+	lst = NULL;
+	get_state(idx, userinput, &lst);
+	actual_lst_size = ft_lst_size(lst);
+	actual_syntax_validation = syntax_validations(lst);
+
+	close_redirect_stderr_to_dev_null(fd);
+	// ASSERT
+	mu_assert_int_eq(xpect_lst_size, actual_lst_size);
+	mu_assert_int_eq(xpect_syntax_validation, actual_syntax_validation);
+}
+
+MU_TEST(testing_syntax_first_node_REDIRECT_INPUT_space_after)
+{
+	// ARRANGE
+	char			*userinput;
+	int				idx;
+	int				xpect_lst_size;
+	int				actual_lst_size;
+	int				xpect_syntax_validation;
+	int				actual_syntax_validation;
+	int				fd;
+	t_token_list	*lst;
+
+	//ACT
+	fd = 0;
+	open_redirect_stderr_to_dev_null(fd);
+	userinput = ">			                          Mussum|Ipsum";
+	xpect_syntax_validation = 301; // begining with redirect input
+	xpect_lst_size = 4;
+	idx = 0;
+	lst = NULL;
+	get_state(idx, userinput, &lst);
+	actual_lst_size = ft_lst_size(lst);
+	actual_syntax_validation = syntax_validations(lst);
+
+	close_redirect_stderr_to_dev_null(fd);
+	// ASSERT
+	mu_assert_int_eq(xpect_lst_size, actual_lst_size);
+	mu_assert_int_eq(xpect_syntax_validation, actual_syntax_validation);
+}
+
+MU_TEST(testing_syntax_first_node_REDIRECT_INPUT_space_before)
+{
+	// ARRANGE
+	char			*userinput;
+	int				idx;
+	int				xpect_lst_size;
+	int				actual_lst_size;
+	int				xpect_syntax_validation;
+	int				actual_syntax_validation;
+	int				fd;
+	t_token_list	*lst;
+
+	//ACT
+	fd = 0;
+	open_redirect_stderr_to_dev_null(fd);
+	userinput = "			                          >Mussum|Ipsum";
+	xpect_syntax_validation = 301; // begining with redirect input
+	xpect_lst_size = 4;
+	idx = 0;
+	lst = NULL;
+	get_state(idx, userinput, &lst);
+	actual_lst_size = ft_lst_size(lst);
+	actual_syntax_validation = syntax_validations(lst);
+
+	close_redirect_stderr_to_dev_null(fd);
+	// ASSERT
+	mu_assert_int_eq(xpect_lst_size, actual_lst_size);
+	mu_assert_int_eq(xpect_syntax_validation, actual_syntax_validation);
+}
+
+
 
 MU_TEST(testing_syntax_first_node_close_parentis_simple_space)
 {
@@ -1049,6 +1142,9 @@ MU_TEST_SUITE(test07___syntax_first_node_errors_suite)
 	MU_RUN_TEST(testing_syntax_first_node_AND_simple_space);
 	MU_RUN_TEST(testing_syntax_first_node_AND_space_after);
 	MU_RUN_TEST(testing_syntax_first_node_AND_space_before);
+	MU_RUN_TEST(testing_syntax_first_node_REDIRECT_INPUT_simple_space);
+	MU_RUN_TEST(testing_syntax_first_node_REDIRECT_INPUT_space_after);
+	MU_RUN_TEST(testing_syntax_first_node_REDIRECT_INPUT_space_before);
 	MU_RUN_TEST(testing_syntax_first_node_close_parentis_simple_space);
 	MU_RUN_TEST(testing_syntax_first_node_close_parentis_space_after);
 	MU_RUN_TEST(testing_syntax_first_node_close_parentis_space_before);
