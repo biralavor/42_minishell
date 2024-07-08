c2d042f1b959480401a903ccdd100254f6fbc7a0
biralavor <umeneses@student.42sp.org.br>
2024-07-08 18:18:40 -0300

    adding redirect heredoc - last node tests

--- _ci_cd/test_files/test09_syntax_state300.c
+++ _ci_cd/test_files/test09_syntax_state300.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 11:01:23 by umeneses          #+#    #+#             */
-/*   Updated: 2024/07/08 18:17:00 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/07/08 18:18:24 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -645,6 +645,96 @@ MU_TEST(testing_syntax_state300_REDIRECT_HEREDOC_space_before_first_node)
 	mu_assert_int_eq(xpect_syntax_validation, actual_syntax_validation);
 }
 
+MU_TEST(testing_syntax_state300_REDIRECT_HEREDOC_simple_space_last_node)
+{
+	// ARRANGE
+	char			*userinput;
+	int				idx;
+	int				xpect_lst_size;
+	int				actual_lst_size;
+	int				xpect_syntax_validation;
+	int				actual_syntax_validation;
+	int				fd;
+	t_token_list	*lst;
+
+	//ACT
+	fd = 0;
+	open_redirect_stderr_to_dev_null(fd);
+	userinput = "Mussum|Ipsum<<";
+	xpect_syntax_validation = 300; // ending with redirect heredoc
+	xpect_lst_size = 4;
+	idx = 0;
+	lst = NULL;
+	get_state(idx, userinput, &lst);
+	actual_lst_size = ft_lst_size(lst);
+	actual_syntax_validation = syntax_validations(lst);
+
+	close_redirect_stderr_to_dev_null(fd);
+	// ASSERT
+	mu_assert_int_eq(xpect_lst_size, actual_lst_size);
+	mu_assert_int_eq(xpect_syntax_validation, actual_syntax_validation);
+}
+
+MU_TEST(testing_syntax_state300_REDIRECT_HEREDOC_space_after_last_node)
+{
+	// ARRANGE
+	char			*userinput;
+	int				idx;
+	int				xpect_lst_size;
+	int				actual_lst_size;
+	int				xpect_syntax_validation;
+	int				actual_syntax_validation;
+	int				fd;
+	t_token_list	*lst;
+
+	//ACT
+	fd = 0;
+	open_redirect_stderr_to_dev_null(fd);
+	userinput = "Mussum|Ipsum<<			                          ";
+	xpect_syntax_validation = 300; // ending with redirect heredoc
+	xpect_lst_size = 4;
+	idx = 0;
+	lst = NULL;
+	get_state(idx, userinput, &lst);
+	actual_lst_size = ft_lst_size(lst);
+	actual_syntax_validation = syntax_validations(lst);
+
+	close_redirect_stderr_to_dev_null(fd);
+	// ASSERT
+	mu_assert_int_eq(xpect_lst_size, actual_lst_size);
+	mu_assert_int_eq(xpect_syntax_validation, actual_syntax_validation);
+}
+
+MU_TEST(testing_syntax_state300_REDIRECT_HEREDOC_space_before_last_node)
+{
+	// ARRANGE
+	char			*userinput;
+	int				idx;
+	int				xpect_lst_size;
+	int				actual_lst_size;
+	int				xpect_syntax_validation;
+	int				actual_syntax_validation;
+	int				fd;
+	t_token_list	*lst;
+
+	//ACT
+	fd = 0;
+	open_redirect_stderr_to_dev_null(fd);
+	userinput = "Mussum|Ipsum			                          <<";
+	xpect_syntax_validation = 300; // ending with redirect heredoc
+	xpect_lst_size = 4;
+	idx = 0;
+	lst = NULL;
+	get_state(idx, userinput, &lst);
+	actual_lst_size = ft_lst_size(lst);
+	actual_syntax_validation = syntax_validations(lst);
+
+	close_redirect_stderr_to_dev_null(fd);
+	// ASSERT
+	mu_assert_int_eq(xpect_lst_size, actual_lst_size);
+	mu_assert_int_eq(xpect_syntax_validation, actual_syntax_validation);
+}
+
 MU_TEST_SUITE(test07___syntax_state300_REDIRECT_INPUT_errors_first_node_suite)
 {
 	MU_RUN_TEST(testing_syntax_state300_REDIRECT_INPUT_simple_space_first_node);
@@ -692,4 +782,11 @@ MU_TEST_SUITE(test07___syntax_state300_REDIRECT_HEREDOC_errors_first_node_suite)
 	MU_RUN_TEST(testing_syntax_state300_REDIRECT_HEREDOC_simple_space_first_node);
 	MU_RUN_TEST(testing_syntax_state300_REDIRECT_HEREDOC_space_after_first_node);
 	MU_RUN_TEST(testing_syntax_state300_REDIRECT_HEREDOC_space_before_first_node);
+}
+
+MU_TEST_SUITE(test07___syntax_state300_REDIRECT_HEREDOC_errors_last_node_suite)
+{
+	MU_RUN_TEST(testing_syntax_state300_REDIRECT_HEREDOC_simple_space_last_node);
+	MU_RUN_TEST(testing_syntax_state300_REDIRECT_HEREDOC_space_after_last_node);
+	MU_RUN_TEST(testing_syntax_state300_REDIRECT_HEREDOC_space_before_last_node);
 }
