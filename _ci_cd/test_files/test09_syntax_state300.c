f07f8129d78953828b3e3ddd0dce08c743baea5b
biralavor <umeneses@student.42sp.org.br>
2024-07-08 18:15:57 -0300

    adding redirect output - last node tests

--- _ci_cd/test_files/test09_syntax_state300.c
+++ _ci_cd/test_files/test09_syntax_state300.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 11:01:23 by umeneses          #+#    #+#             */
-/*   Updated: 2024/07/08 18:13:43 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/07/08 18:15:35 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -272,6 +272,96 @@ MU_TEST(testing_syntax_state300_REDIRECT_OUTPUT_space_before_first_node)
 	open_redirect_stderr_to_dev_null(fd);
 	userinput = "			                          >Mussum|Ipsum";
 	xpect_syntax_validation = 300; // begining with redirect output
+	xpect_lst_size = 4;
+	idx = 0;
+	lst = NULL;
+	get_state(idx, userinput, &lst);
+	actual_lst_size = ft_lst_size(lst);
+	actual_syntax_validation = syntax_validations(lst);
+
+	close_redirect_stderr_to_dev_null(fd);
+	// ASSERT
+	mu_assert_int_eq(xpect_lst_size, actual_lst_size);
+	mu_assert_int_eq(xpect_syntax_validation, actual_syntax_validation);
+}
+
+MU_TEST(testing_syntax_state300_REDIRECT_OUTPUT_simple_space_last_node)
+{
+	// ARRANGE
+	char			*userinput;
+	int				idx;
+	int				xpect_lst_size;
+	int				actual_lst_size;
+	int				xpect_syntax_validation;
+	int				actual_syntax_validation;
+	int				fd;
+	t_token_list	*lst;
+
+	//ACT
+	fd = 0;
+	open_redirect_stderr_to_dev_null(fd);
+	userinput = "Mussum|Ipsum >";
+	xpect_syntax_validation = 300; // ending with redirect output
+	xpect_lst_size = 4;
+	idx = 0;
+	lst = NULL;
+	get_state(idx, userinput, &lst);
+	actual_lst_size = ft_lst_size(lst);
+	actual_syntax_validation = syntax_validations(lst);
+
+	close_redirect_stderr_to_dev_null(fd);
+	// ASSERT
+	mu_assert_int_eq(xpect_lst_size, actual_lst_size);
+	mu_assert_int_eq(xpect_syntax_validation, actual_syntax_validation);
+}
+
+MU_TEST(testing_syntax_state300_REDIRECT_OUTPUT_space_after_last_node)
+{
+	// ARRANGE
+	char			*userinput;
+	int				idx;
+	int				xpect_lst_size;
+	int				actual_lst_size;
+	int				xpect_syntax_validation;
+	int				actual_syntax_validation;
+	int				fd;
+	t_token_list	*lst;
+
+	//ACT
+	fd = 0;
+	open_redirect_stderr_to_dev_null(fd);
+	userinput = "Mussum|Ipsum>			                          ";
+	xpect_syntax_validation = 300; // ending with redirect output
+	xpect_lst_size = 4;
+	idx = 0;
+	lst = NULL;
+	get_state(idx, userinput, &lst);
+	actual_lst_size = ft_lst_size(lst);
+	actual_syntax_validation = syntax_validations(lst);
+
+	close_redirect_stderr_to_dev_null(fd);
+	// ASSERT
+	mu_assert_int_eq(xpect_lst_size, actual_lst_size);
+	mu_assert_int_eq(xpect_syntax_validation, actual_syntax_validation);
+}
+
+MU_TEST(testing_syntax_state300_REDIRECT_OUTPUT_space_before_last_node)
+{
+	// ARRANGE
+	char			*userinput;
+	int				idx;
+	int				xpect_lst_size;
+	int				actual_lst_size;
+	int				xpect_syntax_validation;
+	int				actual_syntax_validation;
+	int				fd;
+	t_token_list	*lst;
+
+	//ACT
+	fd = 0;
+	open_redirect_stderr_to_dev_null(fd);
+	userinput = "Mussum|Ipsum			                          >";
+	xpect_syntax_validation = 300; // ending with redirect output
 	xpect_lst_size = 4;
 	idx = 0;
 	lst = NULL;
