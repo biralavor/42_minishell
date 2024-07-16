100d174db17012985e94f2bf92dc6a40ec41f3e5
umeneses <umeneses@student.42sp.org.br>
2024-07-16 17:17:26 -0300

    applying builtins controller

--- src/02.check_userinput.c
+++ src/02.check_userinput.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 09:20:45 by tmalheir          #+#    #+#             */
-/*   Updated: 2024/07/15 10:02:30 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/07/16 16:32:36 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include "parser.h"
 #include "executor.h"
 #include "error_manager.h"
+#include "builtins.h"
 
 void	check_userinput(char *str)
 {
@@ -26,6 +27,7 @@ void	check_userinput(char *str)
 	if (!create_token_list(str, &lst))
 		error_manager_lexer(LIST_NOT_CREATED);
 	syntax_analysis(lst);
+	builtins_controller(lst);
 	free_token_list(&lst);
 //	initiate_tree(lst);
 }
