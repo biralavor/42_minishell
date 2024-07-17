83fd7a27a94d087e4da42d00d3428320c4f21e92
umeneses <umeneses@student.42sp.org.br>
2024-07-17 12:53:50 -0300

    implementing builtin - CD

--- headers/builtins.h
+++ headers/builtins.h
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 14:57:00 by umeneses          #+#    #+#             */
-/*   Updated: 2024/07/16 18:40:38 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/07/17 12:38:45 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,23 @@ void	builtins_controller(t_token_list *lst);
 bool	builtins_detector(t_token_list *lst);
 
 /**
- * @brief: Runs the echo - imitate words - builtin function.
+ * @brief: Runs the echo - imitate words - builtin command.
 */
 void	builtins_runner_echo(t_token_list *lst);
 
 /**
- * @brief: Runs the cd - change directory - builtin function.
+ * @brief: Runs the pwd - print work directory - builtin command.
 */
 void	builtins_runner_pwd(t_token_list *lst);
+
+/**
+ * @brief: Runs the cd - change directory - builtin command.
+*/
+void	builtins_runner_cd(t_token_list *lst);
+
+/**
+ * @brief: Prints an error message when the cd command fails.
+*/
+void	builtins_error_cd(int destiny_len, char *destiny_path);
 
 #endif
