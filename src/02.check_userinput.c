501d7d3cf102d48349ccb6b3690dfa458564a8ed
umeneses <umeneses@student.42sp.org.br>
2024-07-17 19:14:15 -0300

    forcing a meamory leak to test Github Actions with Valgrind

--- src/02.check_userinput.c
+++ src/02.check_userinput.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   02.check_userinput.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tmalheir <tmalheir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 09:20:45 by tmalheir          #+#    #+#             */
-/*   Updated: 2024/07/16 11:41:02 by tmalheir         ###   ########.fr       */
+/*   Updated: 2024/07/17 19:13:49 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,6 @@ void	check_userinput(char *str)
 	if (!create_token_list(str, &lst))
 		error_manager_lexer(LIST_NOT_CREATED);
 	syntax_analysis(lst);
-	free_token_list(&lst);
+	// free_token_list(&lst);
 //	initiate_tree(lst);
 }
