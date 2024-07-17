bc9d2b0c519a0628a8d223e94395c7b8b42b1b0a
umeneses <umeneses@student.42sp.org.br>
2024-07-17 14:52:31 -0300

    removing duplicated verification

--- src/builtins/check_builtins_echo.c
+++ src/builtins/check_builtins_echo.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 13:23:46 by umeneses          #+#    #+#             */
-/*   Updated: 2024/07/17 14:49:38 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/07/17 14:51:39 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,30 +19,23 @@
 void	builtins_runner_echo(t_token_list *lst)
 {
 	t_token_list	*temp;
-	int				lex_cmd_len;
-	char			*lex_cmd;
 
 	temp = lst;
 	while (temp && temp->type == WORD && NULL != temp->next)
 	{
-		lex_cmd = temp->lexeme;
-		lex_cmd_len = ft_strlen(temp->lexeme);
-		if (ft_strncmp(lex_cmd, "echo", lex_cmd_len) == 0)
+		if (builtins_check_cmd_arg(lst, 'n'))
+			temp = temp->next->next;
+		while (temp->type == WORD)
 		{
-			if (builtins_check_cmd_arg(lst, 'n'))
-				temp = temp->next->next;
-			while (temp->type == WORD)
-			{
-				ft_putstr_fd(temp->lexeme, STDOUT_FILENO);
-				if (NULL == temp->next)
-					break ;
-				else
-					ft_putstr_fd(" ", STDOUT_FILENO);
-				temp = temp->next;
-			}
-			if (!builtins_check_cmd_arg(lst, 'n'))
-				write(1, "\n", 1);
+			ft_putstr_fd(temp->lexeme, STDOUT_FILENO);
+			if (NULL == temp->next)
+				break ;
+			else
+				ft_putstr_fd(" ", STDOUT_FILENO);
+			temp = temp->next;
 		}
+		if (!builtins_check_cmd_arg(lst, 'n'))
+			write(1, "\n", 1);
 	}
 }
 
