ff9003546268ae08458aafa8a177f9106d607a5a
umeneses <umeneses@student.42sp.org.br>
2024-07-17 16:42:03 -0300

    adding TODO message

--- src/builtins/check_builtins_export.c
+++ src/builtins/check_builtins_export.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 13:23:46 by umeneses          #+#    #+#             */
-/*   Updated: 2024/07/17 16:28:46 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/07/17 16:41:39 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,12 @@
 #include "error_manager.h"
 #include "builtins.h"
 
+/**
+ * @brief:
+ * @param var_name the name of the variable to export.
+ * @param var_value the value to be setted in the var_name.
+ * TODO: Use var_name and var_value to set the environment variable.
+*/
 void	builtins_runner_export(t_token_list *lst)
 {
 	t_token_list	*temp;
