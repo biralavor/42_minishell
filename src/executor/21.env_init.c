d23d2fc304afc5d90240d599ab20b2a9f906dcec
umeneses <umeneses@student.42sp.org.br>
2024-08-07 15:04:11 -0300

    adding environment init function

--- src/executor/21.env_init.c
+++ src/executor/21.env_init.c
@@ -6,14 +6,50 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 10:58:32 by umeneses          #+#    #+#             */
-/*   Updated: 2024/08/07 13:29:05 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/08/07 15:01:56 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "executor.h"
 
+/**
+ * @brief Initializes the environment table.
+ * @param envp -> a system array of strings that define the
+ * current environment variables.
+ * While envp is not NULL, it will iterate through the environment
+ * variables and add them to the table. When ft_strchr finds the first '=',
+ * it copies the string before the '=' to KEY (at position equal_sign - *envp)
+ * and the string after the '=' to VALUE.
+ */
+void	environment_init(char **envp)
+{
+	t_env_table	*env_table;
+	char		*key;
+	char		*value;
+	char		*equal_sign;
 
+	env_table = create_table(50);
+	if (!env_table)
+	{
+		ft_putendl_fd("Error: Could not create environment table", 2);
+		exit (EXIT_FAILURE);
+	}
+	while (envp && *envp)
+	{
+		equal_sign = ft_strchr(*envp, '=');
+		if (equal_sign)
+		{
+			key = ft_substr(*envp, 0, equal_sign - *envp);
+			value = ft_strdup(equal_sign + 1);
+			if (key && value)
+				addto_env_table(env_table, key, value);
+			free(key);
+			free(value);
+		}
+		envp++;
+	}
+}
 
 t_env_table	*create_table(int initial_size)
 {
