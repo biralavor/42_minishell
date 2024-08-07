9d95d2092389acc6d7c950a8fd3377b6d75ff415
umeneses <umeneses@student.42sp.org.br>
2024-08-07 13:27:27 -0300

    adding addto environment table function

--- src/executor/21.env_init.c
+++ src/executor/21.env_init.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 10:58:32 by umeneses          #+#    #+#             */
-/*   Updated: 2024/08/07 13:26:19 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/08/07 13:27:06 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,3 +32,23 @@ t_env_table	*create_table(int initial_size)
 	table->size = initial_size;
 	return (table);
 }
+
+void	addto_env_table(t_env_table *table, const char *key, const char *value)
+{
+	int			hash;
+	t_env_entry	*new_entry;
+	float		load_factor;
+
+	hash = hash_function(key, table->size);
+	new_entry = (t_env_entry *)ft_calloc(1, sizeof(t_env_entry));
+	if (!new_entry)
+		return ;
+	new_entry->key = ft_strdup(key);
+	new_entry->value = ft_strdup(value);
+	new_entry->next = table->head[hash];
+	load_factor = (float)table->size / (float)(table->size + 1);
+	if (load_factor >= 0.75 && table->size <= 100)
+		resize_table(table);
+	table->head[hash] = new_entry;
+}
+
