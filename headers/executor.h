f7859303dd4d0ed4da07b2bfddec8d4b39c14e69
umeneses <umeneses@student.42sp.org.br>
2024-08-15 14:38:06 -0300

    adding functions brief

--- headers/executor.h
+++ headers/executor.h
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 13:56:37 by tmalheir          #+#    #+#             */
-/*   Updated: 2024/08/15 14:31:38 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/08/15 14:36:58 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,14 +54,43 @@ typedef struct s_tree
  * and the string after the '=' to VALUE.
  */
 void			environment_init(char **envp);
+
+/**
+ * @brief Allocates memory for the environment table, by a size.
+ */
 t_env_entry		*alloc_table(int init_size);
+
+/**
+ * @brief Adds a new entry to the environment table. If the table is NULL,
+ * then it returns the new_entry. Otherwise, it will iterate through the table.
+ */
 t_env_entry		*addto_env_table(t_env_entry *table, const char *key,
 					const char *value);
-t_env_entry		*env_holder(t_env_entry *table, bool update, bool clear);
+
+/**
+ * @brief Removes an entry from the environment table.
+ * @param table -> the environment variables.
+ * @param update -> if true, updates the static environment table.
+ * @param clear_table -> if true, clears the static environment table.
+ */
+t_env_entry		*env_holder(t_env_entry *table, bool update, bool clear_table);
+
+/**
+ * @brief: Goes to the head of the environment table.
+ */
 t_env_entry		*goto_head_env_table(t_env_entry *table);
+
+/**
+ * @brief: Goes to the end of the environment table.
+ */
 t_env_entry		*goto_end_env_table(t_env_entry *table);
 
 /* ENVIRONMENT UTILS FUNCTIONS */
+/**
+ * @brief: Looks up for a key in the environment table.
+ * @param table -> the environment variables.
+ * @param key -> the key to be searched.
+ */
 t_env_entry		*lookup_table(t_env_entry *table, char *key);
 
 /**
@@ -74,8 +103,11 @@ t_env_entry		*lookup_table(t_env_entry *table, char *key);
  * to ensure the hash value fits within the bounds of the table.
  */
 unsigned long	hash_maker(const char *key, int size);
+
 void			ft_env_printer(t_env_entry	*env_table);
+
 void			free_env_table(t_env_entry *env_vars);
+
 void			free_env_array(char **envp);
 
 /*TREE BUILDING FUNCTIONS*/
