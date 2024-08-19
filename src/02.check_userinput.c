575b1ecb9e5fda9437f9e8a3dd5a0358c4560980
umeneses <umeneses@student.42sp.org.br>
2024-08-19 15:08:51 -0300

    applying norminette

--- src/02.check_userinput.c
+++ src/02.check_userinput.c
@@ -6,30 +6,30 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 09:20:45 by tmalheir          #+#    #+#             */
-/*   Updated: 2024/08/17 12:55:57 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/08/19 15:05:39 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+/*
+void	check_userinput(char *str)
+{
+	t_token_list	*lst;
+	t_tree			*token_tree;
 
-// void	check_userinput(char *str)
-// {
-// 	t_token_list	*lst;
-// 	t_tree			*token_tree;
-
-// 	lst = NULL;
-// 	token_tree = NULL;
-// 	if (!check_initial_errors(str))
-// 		error_manager_lexer(INITIAL_ERROR);
-// 	if (!create_token_list(str, &lst))
-// 		error_manager_lexer(LIST_NOT_CREATED);
-// 	syntax_analysis(lst);
-// 	// builtins_manager(lst);
-// 	token_tree = initiate_tree(lst);
-// 	// ft_tree_printer(token_tree);
-// 	free_token_tree(token_tree);
-// }
-
+	lst = NULL;
+	token_tree = NULL;
+	if (!check_initial_errors(str))
+		error_manager_lexer(INITIAL_ERROR);
+	if (!create_token_list(str, &lst))
+		error_manager_lexer(LIST_NOT_CREATED);
+	syntax_analysis(lst);
+	// builtins_manager(lst);
+	token_tree = initiate_tree(lst);
+	// ft_tree_printer(token_tree);
+	free_token_tree(token_tree);
+}
+*/
 
 void	loop_routine(char *str)
 {
@@ -45,6 +45,5 @@ void	loop_routine(char *str)
 	syntax_analysis(lst);
 	token_tree = initiate_tree(lst);
 	tree_execution(token_tree);
-
 	free_token_tree(token_tree);
 }
