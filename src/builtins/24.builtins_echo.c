df724e5046154d660760c4c131bc4e5d966395bc
biralavor <umeneses@student.42sp.org.br>
2024-08-19 08:04:07 -0300

    fixing with new function parameter - char **cmd

--- src/builtins/24.builtins_echo.c
+++ src/builtins/24.builtins_echo.c
@@ -6,46 +6,43 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 13:23:46 by umeneses          #+#    #+#             */
-/*   Updated: 2024/08/16 09:45:29 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/08/18 14:39:25 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	builtins_runner_echo(t_token_list *lst)
+void	builtins_runner_echo(char **cmd)
 {
-	t_token_list	*temp;
+	int	idx;
 
-	temp = lst;
-	if (NULL == temp->next)
+	idx = 1;
+	if (cmd[idx + 1] == NULL)
 		write(1, "\n", 1);
 	else
 	{
-		if (check_command_args(lst, 'n'))
-			temp = temp->next->next;
-		while (temp->type == WORD)
+		if (check_command_args(cmd, "-n"))
+			idx++;
+		while (cmd[idx] != NULL)
 		{
-			ft_putstr_fd(temp->lexeme, STDOUT_FILENO);
-			if (NULL == temp->next)
+			ft_putstr_fd(cmd[idx], STDOUT_FILENO);
+			if (NULL == cmd[idx + 1])
 				break ;
 			else
 				ft_putstr_fd(" ", STDOUT_FILENO);
-			temp = temp->next;
+			idx++;
 		}
-		if (!check_command_args(lst, 'n'))
+		if (!check_command_args(cmd, "-n"))
 			write(1, "\n", 1);
 	}
 }
 
-bool	check_command_args(t_token_list *lst, char arg)
+bool	check_command_args(char **cmd, char *arg)
 {
-	t_token_list	*temp;
+	int		idx;
 
-	temp = lst;
-	if (temp->next->lexeme[0] == '-')
-	{
-		if (temp->next->lexeme[1] == arg)
-			return (true);
-	}
+	idx = 1;
+	if (ft_strncmp(cmd[idx], arg, 2) == 0)
+		return (true);
 	return (false);
 }
