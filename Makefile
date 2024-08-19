71ab699e1df7653cc9651146d98cf6fb015e445a
umeneses <umeneses@student.42sp.org.br>
2024-08-19 11:32:43 -0300

    moving function to new file

--- Makefile
+++ Makefile
@@ -76,6 +76,7 @@ SRC_FILES				+= ./builtins/27.builtins_export.c
 SRC_FILES				+= ./builtins/28.builtins_export_utils.c
 SRC_FILES				+= ./builtins/29.builtins_unset.c
 SRC_FILES				+= ./builtins/30.builtins_env.c
+SRC_FILES				+= ./builtins/31.builtins_utils.c
 SRC_FILES				+= ./executor/31.execve.c
 
 #SRC_FILES				= minishell_main.c
--- src/builtins/31.builtins_utils.c
+++ src/builtins/31.builtins_utils.c
@@ -1,40 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   24.builtins_echo.c                                 :+:      :+:    :+:   */
+/*   23.builtins_utils.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 13:23:46 by umeneses          #+#    #+#             */
-/*   Updated: 2024/08/19 11:30:00 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/08/19 11:30:47 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static bool	arg_detector;
-
-void	builtins_runner_echo(t_token_list *lst)
+bool	checking_cmd_arg_options(char *lexeme, char *arg)
 {
-	t_token_list	*cmd;
-
-	cmd = lst->next;
-	if (NULL == cmd)
+	if (ft_strncmp(lexeme, arg, 2) == 0)
 	{
-		write(1, "\n", 1);
-		return ;
+		arg_detector = true;
+		return (true);
 	}
-	if (checking_cmd_arg_options(cmd->lexeme, "-n"))
-		cmd = cmd->next;
-	while (cmd)
-	{
-		ft_putstr_fd(cmd->lexeme, STDOUT_FILENO);
-		if (NULL == cmd->next)
-			break ;
-		else
-			ft_putstr_fd(" ", STDOUT_FILENO);
-		cmd = cmd->next;
-	}
-	if (!arg_detector)
-		write(1, "\n", 1);
+	return (false);
 }
