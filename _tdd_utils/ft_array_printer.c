915ba2fa7cecc59740448011567bddc55273eea4
umeneses <umeneses@student.42sp.org.br>
2024-08-20 17:39:30 -0300

    improving array printer feedback

--- _tdd_utils/ft_array_printer.c
+++ _tdd_utils/ft_array_printer.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 14:14:30 by umeneses          #+#    #+#             */
-/*   Updated: 2024/08/16 14:26:59 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/08/20 17:38:32 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,11 @@ void	ft_array_printer(char **array)
 	int	idx;
 
 	idx = 0;
+	ft_printf(YELLOW"Printing Commands Array:\n");
 	while (array[idx])
 	{
-		ft_printf("[%d] = %s\n", idx, array[idx]);
+		ft_printf(CYAN"[%d] = %s\n", idx, array[idx]);
 		idx++;
 	}
+	ft_printf(RESET"\n\n");
 }
