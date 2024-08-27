70c99d7e235aac601e7a245d41415d58f81421cf
umeneses <umeneses@student.42sp.org.br>
2024-08-27 09:42:25 -0300

    removing unnecessary parameter, and fixing env

--- src/builtins/30.builtins_env.c
+++ src/builtins/30.builtins_env.c
@@ -6,19 +6,21 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 13:23:46 by umeneses          #+#    #+#             */
-/*   Updated: 2024/08/16 09:45:53 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/08/27 09:13:37 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	builtins_runner_env(t_env_entry *env_vars)
+void	builtins_runner_env(void)
 {
-	char	*var_key;
-	char	*var_value;
+	char		*var_key;
+	char		*var_value;
+	t_env_entry	*env_vars;
 
 	var_key = NULL;
 	var_value = NULL;
+	env_vars = env_holder(NULL, false, false);
 	if (!env_vars)
 		return ;
 	else
