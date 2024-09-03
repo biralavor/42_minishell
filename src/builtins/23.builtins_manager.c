7dc80ab74f69ee4fb4b5fbe5705a55ee4a2ec4ba
biralavor <umeneses@student.42sp.org.br>
2024-09-03 00:24:17 -0300

    removing hardcode

--- src/builtins/23.builtins_manager.c
+++ src/builtins/23.builtins_manager.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 13:23:46 by umeneses          #+#    #+#             */
-/*   Updated: 2024/09/02 19:13:46 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/09/03 00:23:37 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,9 +59,9 @@ bool	builtins_detector_with_possible_args(t_token_list *lst)
 	cmd = lst;
 	while (cmd && cmd->type == WORD)
 	{
-		if ((ft_strncmp(cmd->lexeme, "export", 6) == 0)
-			|| (ft_strncmp(cmd->lexeme, "unset", 5) == 0)
-			|| (ft_strncmp(cmd->lexeme, "env", 3) == 0))
+		if ((ft_strncmp(cmd->lexeme, "export", ft_strlen(cmd->lexeme)) == 0)
+			|| (ft_strncmp(cmd->lexeme, "unset", ft_strlen(cmd->lexeme)) == 0)
+			|| (ft_strncmp(cmd->lexeme, "env", ft_strlen(cmd->lexeme)) == 0))
 			return (true);
 		cmd = cmd->next;
 	}
@@ -75,19 +75,19 @@ void	builtins_with_possible_args_manager(t_token_list *lst)
 	cmd = lst;
 	while (cmd)
 	{
-		if (ft_strncmp(cmd->lexeme, "export", 6) == 0)
+		if (ft_strncmp(cmd->lexeme, "export", ft_strlen(cmd->lexeme)) == 0)
 		{
 			if (cmd->next && cmd->next->type == WORD)
 				builtins_runner_export(cmd->next->lexeme);
 			else
 				builtins_runner_export(NULL);
 		}
-		else if (ft_strncmp(cmd->lexeme, "unset", 5) == 0)
+		else if (ft_strncmp(cmd->lexeme, "unset", ft_strlen(cmd->lexeme)) == 0)
 		{
 			if (cmd->next && cmd->next->type == WORD)
 				builtins_runner_unset(cmd->next->lexeme);
 		}
-		else if (ft_strncmp(cmd->lexeme, "env", 3) == 0)
+		else if (ft_strncmp(cmd->lexeme, "env", ft_strlen(cmd->lexeme)) == 0)
 			builtins_runner_env();
 		cmd = cmd->next;
 	}
