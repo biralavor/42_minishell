f1980be718b7b2feb6eac8315093d56f9fb3665e
umeneses <umeneses@student.42sp.org.br>
2024-09-09 16:33:45 -0300

    applying expansion tilde for home

--- src/expansions/expansion_manager.c
+++ src/expansions/expansion_manager.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 18:31:10 by umeneses          #+#    #+#             */
-/*   Updated: 2024/09/09 13:40:54 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/09/09 16:31:58 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ void	expansion_manager(t_token_list *cmd)
 	{
 		if (expansion_quotes_detector_at_middle(tmp))
 			tmp->lexeme = expansion_quotes_runner_at_middle(tmp->lexeme);
+		if (expansion_tilde_detector(tmp->lexeme))
+			tmp->lexeme = expansion_tilde_to_home(tmp->lexeme);
 		if (expansion_detector_at_start(tmp))
 		{
 			if (expansion_env_var_detector_at_start(tmp->lexeme[1]))
