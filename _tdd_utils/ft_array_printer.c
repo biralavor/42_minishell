6039586fe860cf46f5316b3e861abc44d41006b3
umeneses <umeneses@student.42sp.org.br>
2024-09-09 12:29:52 -0300

    switching fd to stderror for array printer

--- _tdd_utils/ft_array_printer.c
+++ _tdd_utils/ft_array_printer.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 14:14:30 by umeneses          #+#    #+#             */
-/*   Updated: 2024/08/29 15:39:21 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/09/09 12:26:42 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ void	ft_array_printer(char **array)
 	int	idx;
 
 	idx = 0;
-	ft_printf(YELLOW"Printing Commands Array:\n");
+	fprintf(stderr, YELLOW"Printing Commands Array:\n");
 	while (array[idx])
 	{
-		ft_printf(CYAN"[%d] = %s\n", idx, array[idx]);
+		fprintf(stderr, CYAN"[%d] = %s\n", idx, array[idx]);
 		idx++;
 	}
-	ft_printf("end_of_array_printing__________________________");
-	ft_printf("here_is_the_command_result:\n"RESET);
+	fprintf(stderr, "end_of_array_printing__________________________");
+	fprintf(stderr, "here_is_the_command_result:\n"RESET);
 }
