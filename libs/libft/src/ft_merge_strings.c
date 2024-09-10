dd4caa1f5f32eefeaad07545a85d101e5f2927a1
biralavor <umeneses@student.42sp.org.br>
2024-09-09 23:47:22 -0300

    LIBFT - adding ft_merge_strings

---
+++ libs/libft/src/ft_merge_strings.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_merge_strings.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/09 23:23:53 by umeneses          #+#    #+#             */
+/*   Updated: 2024/09/09 23:24:35 by umeneses         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_merge_strings(char *str1, char *str2)
+{
+	char	*merged;
+	merged = ft_strjoin(str1, str2);
+	if (str1)
+		free(str1);
+	if (str2)
+		free(str2);
+	return (merged);
+}
