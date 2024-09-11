28fc7abad5cf8d0a3f8cf6fa21ef854a05a607bf
umeneses <umeneses@student.42sp.org.br>
2024-09-11 16:18:37 -0300

    moving functions to new files

--- src/executor/env_var_rules.c
+++ src/executor/env_var_rules.c
@@ -1,49 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   expansion_detectors_middle.c                       :+:      :+:    :+:   */
+/*   env_var_rules.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/26 18:31:10 by umeneses          #+#    #+#             */
-/*   Updated: 2024/09/11 13:56:54 by umeneses         ###   ########.fr       */
+/*   Created: 2024/09/11 16:07:10 by umeneses          #+#    #+#             */
+/*   Updated: 2024/09/11 16:07:27 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-bool	expansion_dollar_sign_detector(t_token_list *cmd)
-{
-	int	idx;
-
-	idx = 0;
-	while (cmd->lexeme[idx])
-	{
-		if (cmd->lexeme[idx] == '$')
-			return (true);
-		idx++;
-	}
-	return (false);
-}
-
-bool	expansion_env_var_detector(char *lexeme)
-{
-	int	idx;
-
-	idx = 1;
-	if (env_var_key_rules_at_start(lexeme[idx]))
-	{
-		idx++;
-		while (lexeme[idx])
-		{
-			if (env_var_key_rules_at_middle(lexeme[idx]))
-				if (lexeme[idx + 1] == '\0')
-					return (true);
-			idx++;
-		}
-	}
-	return (false);
-}
 
 bool	env_var_key_rules_at_start(char c)
 {
@@ -55,13 +22,6 @@ bool	env_var_key_rules_at_start(char c)
 bool	env_var_key_rules_at_middle(char c)
 {
 	if (ft_isdigit(c) || ft_isalpha(c) || c == '_')
-		return (true);
-	return (false);
-}
-
-bool	expansion_quotes_detector(t_token_list *cmd)
-{
-	if (cmd->type == DOUBLE_QUOTES || cmd->type == SINGLE_QUOTES)
 		return (true);
 	return (false);
 }
--- src/expansions/expansion_detectors.c
+++ src/expansions/expansion_detectors.c
@@ -1,27 +1,53 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   expansion_detectors_start.c                        :+:      :+:    :+:   */
+/*   expansion_detectors.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 18:31:10 by umeneses          #+#    #+#             */
-/*   Updated: 2024/09/11 15:58:15 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/09/11 16:10:24 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-bool	expansion_detector_at_start(t_token_list *cmd)
+bool	expansion_dollar_sign_detector(t_token_list *cmd)
 {
-	if (cmd->type == WORD && cmd->lexeme[0] == '$')
-		return (true);
+	int	idx;
+
+	idx = 0;
+	while (cmd->lexeme[idx])
+	{
+		if (cmd->lexeme[idx] == '$')
+			return (true);
+		idx++;
+	}
 	return (false);
 }
 
-bool	expansion_env_var_detector_at_start(char lexeme_char_pos)
+bool	expansion_env_var_detector(char *lexeme)
 {
-	if (ft_isalpha(lexeme_char_pos) || lexeme_char_pos == '_')
+	int	idx;
+
+	idx = 1;
+	if (env_var_key_rules_at_start(lexeme[idx]))
+	{
+		idx++;
+		while (lexeme[idx])
+		{
+			if (env_var_key_rules_at_middle(lexeme[idx]))
+				if (lexeme[idx + 1] == '\0')
+					return (true);
+			idx++;
+		}
+	}
+	return (false);
+}
+
+bool	expansion_quotes_detector(t_token_list *cmd)
+{
+	if (cmd->type == DOUBLE_QUOTES || cmd->type == SINGLE_QUOTES)
 		return (true);
 	return (false);
 }
@@ -33,16 +59,9 @@ bool	expansion_question_mark_detector(char *lexeme)
 	return (false);
 }
 
-// bool	expansion_wildcard_detector(t_token_list *lst)
+// bool	expansion_wildcard_detector(char *lexeme)
 // {
-// 	t_token_list	*tmp;
-
-// 	tmp = lst;
-// 	while (tmp)
-// 	{
-// 		if (lst->type == WORD && lst->lexeme[0] == '$' && lst->lexeme[1] == '*')
-// 			return (true);
-// 		tmp = tmp->next;
-// 	}
+// 	if (lexeme[1] == '*' && lexeme[2] == '\0')
+// 		return (true);
 // 	return (false);
 // }
