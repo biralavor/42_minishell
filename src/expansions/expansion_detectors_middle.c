875ad897ef6c6d45e3b3327c28d36643492df4b9
umeneses <umeneses@student.42sp.org.br>
2024-09-11 16:03:26 -0300

    applying team's code pattern

--- src/expansions/expansion_detectors_middle.c
+++ src/expansions/expansion_detectors_middle.c
@@ -6,13 +6,13 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 18:31:10 by umeneses          #+#    #+#             */
-/*   Updated: 2024/09/09 14:24:22 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/09/11 13:56:54 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-bool	expansion_detector_at_middle(t_token_list *cmd)
+bool	expansion_dollar_sign_detector(t_token_list *cmd)
 {
 	int	idx;
 
@@ -26,25 +26,40 @@ bool	expansion_detector_at_middle(t_token_list *cmd)
 	return (false);
 }
 
-bool	expansion_env_var_detector_at_middle(t_token_list *cmd)
+bool	expansion_env_var_detector(char *lexeme)
 {
 	int	idx;
 
-	idx = 0;
-	while (cmd->lexeme[idx])
+	idx = 1;
+	if (env_var_key_rules_at_start(lexeme[idx]))
 	{
-		if (cmd->lexeme[idx] == '$')
-		{
-			idx++;
-			if (ft_isalpha(cmd->lexeme[idx]) || cmd->lexeme[idx] == '_')
-				return (true);
-		}
 		idx++;
+		while (lexeme[idx])
+		{
+			if (env_var_key_rules_at_middle(lexeme[idx]))
+				if (lexeme[idx + 1] == '\0')
+					return (true);
+			idx++;
+		}
 	}
 	return (false);
 }
 
-bool	expansion_quotes_detector_at_middle(t_token_list *cmd)
+bool	env_var_key_rules_at_start(char c)
+{
+	if (ft_isalpha(c) || c == '_')
+		return (true);
+	return (false);
+}
+
+bool	env_var_key_rules_at_middle(char c)
+{
+	if (ft_isdigit(c) || ft_isalpha(c) || c == '_')
+		return (true);
+	return (false);
+}
+
+bool	expansion_quotes_detector(t_token_list *cmd)
 {
 	if (cmd->type == DOUBLE_QUOTES || cmd->type == SINGLE_QUOTES)
 		return (true);
