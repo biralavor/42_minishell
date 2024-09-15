8e2d2a794a21406754360e2c520f5344730222bb
biralavor <umeneses@student.42sp.org.br>
2024-09-15 14:55:53 -0300

    improving code readability

--- src/06_builtins/44.builtin_export_utils.c
+++ src/06_builtins/44.builtin_export_utils.c
@@ -1,38 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   28.builtins_export_utils.c                         :+:      :+:    :+:   */
+/*   44.builtin_export_utils.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 13:23:46 by umeneses          #+#    #+#             */
-/*   Updated: 2024/09/06 17:02:52 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/09/15 14:02:23 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_env_entry	*arg_handle_runner(t_env_entry *env_vars, char *arg)
+void	arg_handle_runner(t_env_entry *env_vars, char *arg)
 {
 	char		*var_key;
 	char		*var_value;
 	char		*equal_sign;
-	t_env_entry	*updated_table;
 
-	updated_table = NULL;
 	equal_sign = ft_strchr(arg, '=');
 	if (equal_sign)
 	{
 		var_key = ft_substr(arg, 0, equal_sign - arg);
 		var_value = ft_strdup(equal_sign + 1);
-		updated_table = addto_env_table(env_vars, var_key, var_value);
-		free(var_key);
-		free(var_value);
 	}
 	else
-		updated_table = addto_env_table(env_vars, arg, "");
-	env_holder(updated_table, true, false);
-	return (updated_table);
+	{
+		var_key = ft_strdup(arg);
+		var_value = ft_strdup("");
+	}
+	addto_env_table(&env_vars, create_new_entry(var_key, var_value, env_vars->size));
+	free(var_key);
+	free(var_value);
+	// env_holder(env_vars, true, false);
 }
 
 int	arg_handle_state_detector(int state, char *arg)
