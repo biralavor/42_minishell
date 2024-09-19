0eb0a49e9a0f649ba2c312bcc2f85ea7505cba64
umeneses <umeneses@student.42sp.org.br>
2024-09-19 20:43:58 -0300

    fixing expansion files

--- headers/expansion.h
+++ headers/expansion.h
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 13:55:02 by umeneses          #+#    #+#             */
-/*   Updated: 2024/09/17 17:20:48 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/09/19 13:09:25 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,17 +24,17 @@ typedef struct s_new_lex
 void	expansion_manager(t_token_list *cmd);
 int		next_char_counter(t_token_list *cmd);
 
-bool	expansion_quotes_detector(t_token_list *cmd);
-char	*expansion_quotes_manager(t_token_list *cmd);
+bool	quotes_detector(t_token_list *cmd);
+char	*remove_quotes_manager(t_token_list *cmd);
 char	*expansion_boundary_quotes_runner(char *lexeme, char *new_lexeme);
 char	*expansion_inside_quotes_runner(t_token_list *cmd, char *new_lexeme);
-void	expansion_merging_tokens(t_token_list *cmd);
+void	expansion_merging_tokens(t_token_list *cmd, char *new_lexeme);
 
 bool	expansion_tilde_detector(char *lexeme);
 char	*expansion_tilde_to_home(char *lexeme);
 
 bool	expansion_dollar_sign_detector(t_token_list *cmd);
-char	*expansion_env_var_runner(char *lexeme);
+char	*expansion_env_var_runner(char *lexeme, int type);
 bool	expansion_env_var_detector(char *lexeme);
 bool	env_var_key_rules_at_start(char c);
 bool	env_var_key_rules_at_middle(char c);
@@ -44,6 +44,8 @@ char	**expand_var_from_array(char **array_lexeme);
 char	**send_approved_var(char **new_arr, char *lexeme);
 char	*merging_array_lexeme(char **array_lexeme);
 char	**free_runner_for_env_rules_manager(char **arr_lex, char **new_arr);
+bool	single_quote_detector(char *lexeme);
+char	*single_quote_remover(char *lexeme);
 
 bool	expansion_question_mark_detector(char *lexeme);
 char	*expansion_question_mark(char *lexeme);
--- src/05_expansions/32.expansion_quotes.c
+++ src/05_expansions/32.expansion_quotes.c
@@ -6,107 +6,29 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 13:41:21 by umeneses          #+#    #+#             */
-/*   Updated: 2024/09/17 17:20:36 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/09/19 19:44:23 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	*expansion_quotes_manager(t_token_list *cmd)
+bool	single_quote_detector(char *lexeme)
 {
-	char	*new_lexeme;
-	int		len;
-	t_token_list *tmp;
-
-	len = 1;
-	tmp = cmd->prev;
-	while (tmp->next_char)
-	{
-		len += ft_strlen(tmp->lexeme);
-		tmp = tmp->next;
-	}
-	if (tmp->next_char == '\0' || tmp->next_char == ' ')
-		len += ft_strlen(tmp->lexeme) - 2;
-	new_lexeme = malloc(sizeof(char) * len);
-	if (new_lexeme == NULL)
-		exit(exit_status_holder(1, true));
-	new_lexeme = expansion_inside_quotes_runner(cmd->prev, new_lexeme);
-	cmd->lexeme = new_lexeme;
-	return (NULL);
+	if (lexeme[0] == '\'')
+			return (true);
+	return (false);
 }
 
-char	*expansion_inside_quotes_runner(t_token_list *cmd, char *new_lexeme)
+char	*single_quote_remover(char *lex)
 {
 	int		idx;
-	int		idx_new;
-	char	quote_type;
 
-	idx = -1;
-	idx_new = -1;
-	quote_type = '\0';
-	if (cmd->next_char)
-		expansion_merging_tokens(cmd);
-	while (cmd->lexeme[++idx])
+	idx = 0;
+	while (lex[idx])
 	{
-		if (quote_type == '\0' && (cmd->lexeme[idx] == '"' || cmd->lexeme[idx] == '\''))
-		{
-			quote_type = cmd->lexeme[idx];
-			idx++;
-		}
-		if (quote_type == cmd->lexeme[idx])
-		{
-			quote_type = '\0';
-			idx++;
-		}
-		new_lexeme[++idx_new] = cmd->lexeme[idx];
+		if (lex[idx] == '\'')
+			ft_memmove(&lex[idx], &lex[idx + 1], ft_strlen(&lex[idx + 1]) + 1);
+		idx++;
 	}
-	new_lexeme[++idx_new] = '\0';
-	free(cmd->lexeme);
-	return (new_lexeme);
-}
-
-void	expansion_merging_tokens(t_token_list *cmd)
-{
-	char		*last_lex;
-	char		*merged_lex;
-	t_token_list *tmp;
-	t_token_list *saved_ptr;
-
-	tmp = cmd;
-	saved_ptr = cmd->prev;
-	while(tmp)
-	{
-		if (tmp->next_char && (tmp->next->next_char == ' '
-			|| tmp->next->next_char == '\0'))
-			break ;
-		tmp = tmp->next;
-	}
-	while(tmp)
-	{
-		if (tmp == saved_ptr)
-		{
-			cmd->lexeme = merged_lex;
-			break ;
-		}
-		if (tmp->lexeme)
-		{	
-			if (merged_lex)
-				last_lex = ft_strjoin(tmp->lexeme, merged_lex);
-			else if (tmp->next->lexeme)
-				last_lex = ft_strjoin(tmp->lexeme, tmp->next->lexeme);
-			free(tmp->lexeme);
-			free(tmp->next->lexeme);
-			tmp->lexeme = NULL;
-			tmp->next->lexeme = NULL;
-			tmp->next_char = '\0';
-			tmp = tmp->prev;
-			if (tmp)
-			{
-				merged_lex = ft_strjoin(tmp->lexeme, last_lex);
-				free(tmp->lexeme);
-				tmp = tmp->prev;
-			}
-			free(last_lex);
-		}
-	}
+	return (lex);
 }
