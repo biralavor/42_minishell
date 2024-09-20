d222b7c17f51ea017ab121f374b13fec69a610b0
biralavor <umeneses@student.42sp.org.br>
2024-09-20 19:19:20 -0300

    fixing return at lexer errors

--- src/01_lexer/12.error_manager_lexer.c
+++ src/01_lexer/12.error_manager_lexer.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 11:01:38 by tmalheir          #+#    #+#             */
-/*   Updated: 2024/09/20 19:09:56 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/09/20 19:18:34 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,20 @@ void	error_manager_lexer(int error_id)
 void	initial_error(void)
 {
 	ft_putendl_fd("Quoting or parenthesis error", STDERR_FILENO);
-	exit (exit_status_holder(1, true));
+	exit_status_holder(1, true);
+	return ;
 }
 
 void	lexer_error(void)
 {
 	ft_putendl_fd("lexer error", STDERR_FILENO);
-	exit (exit_status_holder(1, true));
+	exit_status_holder(1, true);
+	return ;
 }
 
 void	list_not_created(void)
 {
 	ft_putendl_fd("Failed to create token linked list", STDERR_FILENO);
-	exit(exit_status_holder(1, true));
+	exit_status_holder(1, true);
+	return ;
 }
