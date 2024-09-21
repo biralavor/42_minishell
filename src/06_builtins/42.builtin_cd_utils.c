7045f6658ab01353111840282ab1139dc1b45b69
biralavor <umeneses@student.42sp.org.br>
2024-09-21 15:00:09 -0300

    fixing error message logic

--- src/06_builtins/42.builtin_cd_utils.c
+++ src/06_builtins/42.builtin_cd_utils.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   builtins_cd_utils.c                                :+:      :+:    :+:   */
+/*   42.builtin_cd_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 10:19:07 by umeneses          #+#    #+#             */
-/*   Updated: 2024/09/03 17:26:50 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/09/21 14:56:56 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,17 @@
 
 void	cd_error_msg(int destiny_len, char *destiny_path, int chdir_status)
 {
-	if (!is_path_a_directory(destiny_path))
+	if (chdir_status == -1)
 	{
-		write(2, "bash: cd: ", 11);
-		write(2, destiny_path, destiny_len);
-		write(2, ": Not a directory", 18);
-		write(2, "\n", 1);
+		write(STDERR_FILENO, "bash: cd: ", 10);
+		write(STDERR_FILENO, destiny_path, destiny_len);
+		write(STDERR_FILENO, ": No such file or directory\n", 28);
 	}
-	else if (chdir_status == ENOENT)
+	else if (!is_path_a_directory(destiny_path))
 	{
-		write(2, "bash: cd: ", 11);
-		write(2, destiny_path, destiny_len);
-		write(2, ": No such file or directory", 28);
-		write(2, "\n", 1);
+		write(STDERR_FILENO, "bash: cd: ", 10);
+		write(STDERR_FILENO, destiny_path, destiny_len);
+		write(STDERR_FILENO, ": Not a directory\n", 18);
 	}
 	exit_status_holder(1, true);
 }
@@ -36,7 +34,7 @@ bool	is_path_a_directory(char *path)
 	struct stat	file_system_status;
 
 	if ((stat(path, &file_system_status) == 0)
-		&& (S_ISDIR(file_system_status.st_mode) != 0))
+		&& S_ISDIR(file_system_status.st_mode))
 		return (true);
 	return (false);
 }
