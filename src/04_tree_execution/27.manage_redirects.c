03b24d51b490e58c74433228df5c99d223bc3753
umeneses <umeneses@student.42sp.org.br>
2024-09-24 18:33:31 -0300

    building new redirect method, but still in WIP

--- src/04_tree_execution/27.manage_redirects.c
+++ src/04_tree_execution/27.manage_redirects.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 10:20:13 by tmalheir          #+#    #+#             */
-/*   Updated: 2024/09/24 12:21:07 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/09/24 18:26:45 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,19 @@ static void	apply_redirect(t_tree *tree, int *fd)
 	if (tree->type == REDIR_IN || tree->type == REDIR_HDOC)
 	{
 		if (dup2(*fd, STDIN_FILENO) == -1)
-		exit(exit_status_holder(EXIT_FAILURE, true));
+		{
+			close(*fd);
+			exit(exit_status_holder(EXIT_FAILURE, true));
+		}
 		close(*fd);
 	}
 	if (tree->type == REDIR_OUT || tree->type == REDIR_OUTAPP)
 	{
 		if (dup2(*fd, STDOUT_FILENO) == -1)
-		exit(exit_status_holder(EXIT_FAILURE, true));
+		{
+			close(*fd);
+			exit(exit_status_holder(EXIT_FAILURE, true));
+		}
 		close(*fd);
 	}
 }
@@ -46,25 +52,22 @@ static void	check_if_apply_redirect(t_tree *tree, int *first_redir, int new_fd)
  * @todo change error message in *fd < 0 condition
  * and make it return to the loop routine
  */
-static int	open_redir_file(t_tree *tree, int *fd)
+static int	open_redir_file(char *pathname, int type, int *fd)
 {
-	char	*pathname;
-
-	pathname = ft_strdup(tree->right->command->lexeme);
-	if (tree->type == REDIR_IN || tree->type == REDIR_HDOC)
+	if (type == REDIR_IN || type == REDIR_HDOC)
 		*fd = open(pathname, O_RDONLY);
-	else if (tree->type == REDIR_OUT)
+	else if (type == REDIR_OUT)
 		*fd = open(pathname, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	else if (tree->type == REDIR_OUTAPP)
+	else if (type == REDIR_OUTAPP)
 		*fd = open(pathname, O_WRONLY | O_CREAT | O_APPEND, 0644);
 	if (*fd < 0)
-	{
-		ft_putendl_fd(" No such file or directory", STDERR_FILENO);
-		exit_status_holder(EXIT_FAILURE, true);
-		// printf("TO RECLAMANDO, NÃO ACHEI O ARQUIVO %s\n", pathname);
+	{	
+		perror(pathname);
+		free(pathname);
+		return (exit_status_holder(1, true));
 	}
 	free(pathname);
-	return (*fd);
+	return (exit_status_holder(0, true));
 }
 
 /**
@@ -75,11 +78,15 @@ void	manage_redirect(t_tree *tree, int flag)
 	int				std_fd[2];
 	int				new_fd;
 	static int		first_redir[2];
+	char			*pathname;
 
 	new_fd = -1;
 	if (!flag)
 		change_redir_flag(first_redir);
-	open_redir_file(tree, &new_fd);
+	pathname = ft_strdup(tree->right->command->lexeme);
+	open_redir_file(pathname, tree->type, &new_fd);
+	if (exit_status_holder(0, false) == 1)
+		return ;
 	std_fd[0] = dup(STDIN_FILENO);
 	std_fd[1] = dup(STDOUT_FILENO);
 	if (tree->right && !tree->right->right)
