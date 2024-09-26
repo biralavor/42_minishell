d2ab85538c02674b54b2926743050d91ba9692b6
umeneses <umeneses@student.42sp.org.br>
2024-09-26 09:28:03 -0300

    applying new error msg method for 101 201 and 301

--- src/02_parser/19.error_manager_parser.c
+++ src/02_parser/19.error_manager_parser.c
@@ -3,28 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   19.error_manager_parser.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tmalheir <tmalheir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 11:53:52 by tmalheir          #+#    #+#             */
-/*   Updated: 2024/09/23 11:23:45 by tmalheir         ###   ########.fr       */
+/*   Updated: 2024/09/26 09:26:53 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	error_manager_parser(int error_id)
+void	error_manager_parser(int error_id, char *lex, int syntax_state)
 {
 	if (error_id == UNEXPECTED_TOKEN)
-		unexpected_token_error();
+	{
+		if (!lex && syntax_state == 201)
+			lex = "|";
+		else if (!lex && syntax_state == 301)
+			lex = "newline";
+		unexpected_token_error(lex);
+	}
 	else if (error_id == SYNTAX_ERROR)
 		syntax_error();
 	else if (error_id == COMMAND_NOT_FOUND)
 		command_not_found();
 }
 
-void	unexpected_token_error(void)
+void	unexpected_token_error(char *lex)
 {
-	ft_putendl_fd("syntax error near unexpected token", STDERR_FILENO);
+	ft_putstr_fd(" syntax error near unexpected token `", STDERR_FILENO);
+	ft_putstr_fd(lex, STDERR_FILENO);
+	ft_putendl_fd("'", STDERR_FILENO);
 }
 
 void	syntax_error(void)
