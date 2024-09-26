06ff5b0d0cd4f6b63a02b920c9c3a563932e057d
umeneses <umeneses@student.42sp.org.br>
2024-09-26 14:51:19 -0300

    improving error msg method to get LST->TYPE

--- src/02_parser/19.error_manager_parser.c
+++ src/02_parser/19.error_manager_parser.c
@@ -6,26 +6,28 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 11:53:52 by tmalheir          #+#    #+#             */
-/*   Updated: 2024/09/26 09:26:53 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/09/26 11:14:32 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	error_manager_parser(int error_id, char *lex, int syntax_state)
+void	error_manager_parser(int error_id, t_token_list *lst, int syntax_state)
 {
 	if (error_id == UNEXPECTED_TOKEN)
 	{
-		if (!lex && syntax_state == 201)
-			lex = "|";
-		else if (!lex && syntax_state == 301)
-			lex = "newline";
-		unexpected_token_error(lex);
+		if (!lst->lexeme && syntax_state == 201)
+			lst->lexeme = ft_strdup("|");
+		else if (!lst->lexeme && syntax_state == 301 && !lst->next)
+			lst->lexeme = ft_strdup("newline");
+		else if (!lst->lexeme && syntax_state == 301 && lst->next)
+			lst->lexeme = get_metachar_type_as_str(lst->next->type);
+		unexpected_token_error(lst->lexeme);
 	}
 	else if (error_id == SYNTAX_ERROR)
 		syntax_error();
-	else if (error_id == COMMAND_NOT_FOUND)
-		command_not_found();
+	// else if (error_id == COMMAND_NOT_FOUND)
+	// 	command_not_found();
 }
 
 void	unexpected_token_error(char *lex)
@@ -41,7 +43,35 @@ void	syntax_error(void)
 	ft_putendl_fd("'\\', '&' or ';' detected\n", STDERR_FILENO);
 }
 
-void	command_not_found(void)
+char	*get_metachar_type_as_str(int type)
 {
-	ft_putendl_fd("Command not found.\n", STDERR_FILENO);
+	char *result;
+
+	result = NULL;
+	if (type == PIPE)
+		result = ft_strdup("|");
+	else if (type == AND)
+		result = ft_strdup("&&");
+	else if (type == OR)
+		result = ft_strdup("||");
+	else if (type == DOUBLE_QUOTES)
+		result = ft_strdup("\"");
+	else if (type == SINGLE_QUOTES)
+		result = ft_strdup("'");
+	else if (type == SUBSHELL)
+		result = ft_strdup("()");
+	else if (type == REDIR_OUT)
+		result = ft_strdup(">");
+	else if (type == REDIR_OUTAPP)
+		result = ft_strdup(">>");
+	else if (type == REDIR_IN)
+		result = ft_strdup("<");
+	else if (type == REDIR_HDOC)
+		result = ft_strdup("<<");
+	return (result);
 }
+
+// void	command_not_found(void)
+// {
+// 	ft_putendl_fd("Command not found.\n", STDERR_FILENO);
+// }
