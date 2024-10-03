23fd49999a7045a89ece124c232f90b69c45a484
biralavor <umeneses@student.42sp.org.br>
2024-10-03 12:35:10 -0300

    applying ft_strcmp instead of ft_strNcmp

--- src/06_builtins/38.builtin_manager.c
+++ src/06_builtins/38.builtin_manager.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 13:23:46 by umeneses          #+#    #+#             */
-/*   Updated: 2024/10/02 10:32:02 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/10/03 12:22:38 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ void	builtins_manager(t_token_list *lst)
 	arg_option = false;
 	while (cmd)
 	{
-		if (cmd->lexeme && ft_strncmp(cmd->lexeme, "echo", 4) == 0)
+		if (cmd->lexeme && ft_strcmp(cmd->lexeme, "echo") == 0)
 			builtins_runner_echo(cmd, arg_option);
-		else if (cmd->lexeme && ft_strncmp(cmd->lexeme, "cd", 2) == 0)
+		else if (cmd->lexeme && ft_strcmp(cmd->lexeme, "cd") == 0)
 			builtins_runner_cd(cmd);
-		else if (cmd->lexeme && ft_strncmp(cmd->lexeme, "pwd", 3) == 0)
+		else if (cmd->lexeme && ft_strcmp(cmd->lexeme, "pwd") == 0)
 			builtins_runner_pwd(cmd);
 		cmd = cmd->next;
 	}
@@ -38,19 +38,19 @@ void	builtins_with_possible_args_manager(t_token_list *lst)
 	cmd = lst;
 	while (cmd)
 	{
-		if (ft_strncmp(cmd->lexeme, "export", 6) == 0)
+		if (ft_strcmp(cmd->lexeme, "export") == 0)
 		{
 			if (cmd->next && cmd->next->type == WORD)
 				builtins_runner_export(cmd->next->lexeme);
 			else
 				builtins_runner_export(NULL);
 		}
-		else if (ft_strncmp(cmd->lexeme, "unset", 5) == 0
+		else if (ft_strcmp(cmd->lexeme, "unset") == 0
 			&& cmd->next && cmd->next->type == WORD)
 			builtins_runner_unset(cmd->next->lexeme);
-		else if (ft_strncmp(cmd->lexeme, "env", 3) == 0)
+		else if (ft_strcmp(cmd->lexeme, "env") == 0)
 			builtins_runner_env();
-		else if (ft_strncmp(cmd->lexeme, "exit", 4) == 0)
+		else if (ft_strcmp(cmd->lexeme, "exit") == 0)
 		{
 			builtins_runner_exit(cmd);
 			break ;
