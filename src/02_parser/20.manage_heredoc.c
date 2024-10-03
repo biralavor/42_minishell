823503f06361e317c3e9d0d04c8d43b9a43f2164
biralavor <umeneses@student.42sp.org.br>
2024-10-03 09:52:23 -0300

    improving heredoc feedback, when ctrl+d is activated

--- src/02_parser/20.manage_heredoc.c
+++ src/02_parser/20.manage_heredoc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   20.manage_heredoc.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tmalheir <tmalheir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 18:23:53 by umeneses          #+#    #+#             */
-/*   Updated: 2024/10/01 14:14:09 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/10/03 09:49:04 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@ void	check_heredoc(t_token_list *lst)
 	char			*heredoc_input;
 	char			*delimiter;
 	t_token_list	*tmp;
-
+	int				idx;
+	
+	idx = 0;
 	heredoc_fd = -1;
 	heredoc_input = NULL;
 	delimiter = NULL;
@@ -37,13 +39,17 @@ void	check_heredoc(t_token_list *lst)
 				exit_status_holder(EXIT_FAILURE, true);
 				break ;
 			}
-			heredoc_input = readline(BLUE"(mini)heredoc> "RESET);
-			if (!check_delimiter(delimiter, heredoc_fd, heredoc_input))
+			heredoc_input = readline(BLUE""RESET);
+			idx++;
+			is_heredoc_running(true, true);
+			heredoc_forcing_exit_warning(heredoc_input, delimiter, idx, heredoc_fd);
+			if (!check_delimiter(delimiter, heredoc_fd, heredoc_input, idx))
 				break ;
 		}
 		tmp = tmp->next;
 	}
 	heredoc_fd_reset(&heredoc_fd);
+	is_heredoc_running(false, false);
 	if (delimiter != NULL)
 		free(delimiter);
 }
@@ -65,7 +71,6 @@ void	path_file(t_token_list *lst)
 			tmp->next->type = ARCHIVE;
 			free(tmp->next->lexeme);
 			tmp->next->lexeme = ft_strdup(pathname);
-			is_heredoc_running(true, false);
 		}
 		tmp = tmp->next;
 	}
@@ -73,7 +78,7 @@ void	path_file(t_token_list *lst)
 	free(pathname);
 }
 
-int	check_delimiter(char *delimiter, int fd, char *input)
+int	check_delimiter(char *delimiter, int fd, char *input, int idx)
 {
 	while (input && ft_strncmp(input, delimiter, (ft_strlen(delimiter) - 1)))
 	{
@@ -81,23 +86,26 @@ int	check_delimiter(char *delimiter, int fd, char *input)
 		{
 			write(fd, input, ft_strlen(input));
 			write(fd, "\n", 1);
+			idx++;
 		}
-		is_heredoc_running(true, false);
 		free(input);
+		heredoc_forcing_exit_warning(input, delimiter, idx, fd);
 		input = readline(BLUE"(mini)heredoc> "RESET);
 	}
 	if (input)
 		free(input);
-	return (0);
+	return (exit_status_holder(EXIT_SUCCESS, true));
 }
 
 bool	is_heredoc_running(bool update, bool caller)
 {
 	static bool	heredoc_running;
 
-	if (update && !caller)
-		heredoc_running = update;
+	if (update && caller)
+		heredoc_running = true;
 	else if (!update && caller)
+		return (heredoc_running);
+	else if (!update && !caller)
 		heredoc_running = false;
 	return (heredoc_running);
 }
@@ -111,6 +119,21 @@ void	heredoc_fd_reset(int *heredoc_fd)
 	}
 }
 
+void	heredoc_forcing_exit_warning(char *input, char *delimiter, int idx, int fd)
+{
+	if (input == NULL)
+	{
+		ft_putstr_fd(" here-document at line ", STDERR_FILENO);
+		ft_putstr_fd(ft_itoa(idx), STDERR_FILENO);
+		ft_putstr_fd(" delimited by end-of-file (wanted `", STDERR_FILENO);
+		ft_putstr_fd(delimiter, STDERR_FILENO);
+		ft_putendl_fd("')", STDERR_FILENO);
+		env_holder(NULL, false, true);
+		close(fd);
+		free(input);
+		exit(exit_status_holder(0, true));
+	}
+}
 
 /*
 static char	*path_file(void)
