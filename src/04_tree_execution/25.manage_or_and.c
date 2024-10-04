7c1a65312fb10286d3dd2eba63d4bc274e1740cf
biralavor <umeneses@student.42sp.org.br>
2024-10-04 17:55:33 -0300

    removing exit_status from tree_execution, manage_and manage_or

--- src/04_tree_execution/25.manage_or_and.c
+++ src/04_tree_execution/25.manage_or_and.c
@@ -1,33 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   35.manage_or_and.c                                 :+:      :+:    :+:   */
+/*   25.manage_or_and.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tmalheir <tmalheir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 14:31:24 by tmalheir          #+#    #+#             */
-/*   Updated: 2024/09/10 13:32:44 by tmalheir         ###   ########.fr       */
+/*   Updated: 2024/10/04 18:01:42 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	manage_or(t_tree *tree)
+void	manage_or(t_tree *tree)
 {
-	int	exit_status;
-
-	exit_status = tree_execution(tree->left, 0);
-	if (exit_status)
-		exit_status = tree_execution(tree->right, 0);
-	return (exit_status);
+	tree_execution(tree->left, 0);
+	if (exit_status_holder(0, false) != 0)
+		tree_execution(tree->right, 0);
 }
 
-int	manage_and(t_tree *tree)
+void	manage_and(t_tree *tree)
 {
-	int	exit_status;
-
-	exit_status = tree_execution(tree->left, 0);
-	if (!exit_status)
-		exit_status = tree_execution(tree->right, 0);
-	return (exit_status);
+	tree_execution(tree->left, 0);
+	if (exit_status_holder(0, false) == 0)
+		tree_execution(tree->right, 0);
 }
