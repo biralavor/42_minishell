4ef1c43ef737ce51194751761f57003dd3187887
biralavor <umeneses@student.42sp.org.br>
2024-10-06 13:09:39 -0300

    building new SIGNALS method

--- src/02.signals.c
+++ src/02.signals.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 09:47:06 by umeneses          #+#    #+#             */
-/*   Updated: 2024/10/04 10:53:02 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/10/04 22:53:26 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,61 @@
 
 volatile sig_atomic_t	g_sigmonitor = 0;
 
-void	signals_init(void)
+void	signals_init(bool interactive)
 {
-	static struct sigaction	monitor_sigint;
-	static struct sigaction	monitor_sigquit;
+	struct sigaction	signal;
 
-	monitor_sigint.sa_handler = &signals_manager;
-	monitor_sigint.sa_flags = 0;
-	sigemptyset(&monitor_sigint.sa_mask);
-	sigaction(SIGINT, &monitor_sigint, NULL);
-	// sigprocmask(SIG_BLOCK, &sig_tobe_blocked, NULL);
-	monitor_sigquit.sa_handler = &signals_manager;
-	monitor_sigquit.sa_flags = 0;
-	sigemptyset(&monitor_sigquit.sa_mask);
-	sigaction(SIGQUIT, &monitor_sigquit, NULL);
+	signal.sa_handler = interrupt_signal_runner;
+	sigemptyset(&signal.sa_mask);
+	signal.sa_flags = SA_RESTART;
+	if (sigaction(SIGINT, &signal, NULL) == -1)
+	{
+		perror("sigaction error");
+		exit(exit_status_holder(EXIT_FAILURE, true));
+	}
+	if (interactive)
+	{
+		signal.sa_handler = SIG_IGN;
+		sigemptyset(&signal.sa_mask);
+		signal.sa_flags = SA_RESTART;
+		if (sigaction(SIGQUIT, &signal, NULL) == -1)
+		{
+			perror("sigaction error");
+			exit(exit_status_holder(EXIT_FAILURE, true));
+		}
+		fprintf(stderr, YELLOW"g_sigmonitor em signals init: %d\n"RESET, g_sigmonitor);
+	}
+	else
+	{
+		signal.sa_handler = SIG_DFL;
+	}
+	sigemptyset(&signal.sa_mask);
+	signal.sa_flags = SA_RESTART;
+	if (sigaction(SIGQUIT, &signal, NULL) == -1)
+	{
+		perror("sigaction error");
+		exit(exit_status_holder(EXIT_FAILURE, true));
+	}
 }
 
-void	signals_manager(int sig_nbr)
-{
-	if (sig_nbr == SIGINT)
-		interrupt_signal_runner();
-	else if (sig_nbr == SIGQUIT)
-		quit_signal_blocker();
-}
+// void	signals_manager(int sig_nbr)
+// {
+// 	if (sig_nbr == SIGINT)
+// 		interrupt_signal_runner();
+// 	else if (sig_nbr == SIGQUIT)
+// 		quit_signal_blocker();
+// }
 
-void	interrupt_signal_runner(void)
+void	interrupt_signal_runner(int sig)
 {
+	(void)sig;
 	if (child_process_is_running(false, true))
 		env_holder(NULL, false, true);
 	if (is_heredoc_running(false, true))
+	{
 		close(STDIN_FILENO);
+		close(STDERR_FILENO);	
+	}
 	rl_on_new_line();
 	rl_replace_line("", 0);
 	ft_putstr_fd("\n", STDOUT_FILENO);
@@ -52,40 +77,38 @@ void	interrupt_signal_runner(void)
 	exit_status_holder(130, true);
 }
 
-void	quit_signal_blocker(void)
-{
-	static sigset_t	sig_tobe_blocked;
+// void	quit_signal_blocker(void)
+// {
+// 	// static sigset_t			sig_tobe_blocked;
+// 	static struct sigaction	monitor_sigquit;
 
-	tty_proprieties_manager(true);
-	
-	sigemptyset(&sig_tobe_blocked);
-	sigaddset(&sig_tobe_blocked, SIGQUIT);
-	if (sigismember(&sig_tobe_blocked, SIGQUIT) == 0)
-		sigprocmask(SIG_BLOCK, &sig_tobe_blocked, NULL);
-	else
-		fprintf(stderr, ">>>>>>>>>> erro no sig sigprocmask");
-	// rl_on_new_line();
-	rl_replace_line("", 0);
-	if (!is_after_loop(false))
-		rl_redisplay();
-}
+// 	monitor_sigquit.sa_handler = SIG_IGN;
+// 	monitor_sigquit.sa_flags = SA_RESTART;
+// 	sigemptyset(&monitor_sigquit.sa_mask);
+// 	sigaction(SIGQUIT, &monitor_sigquit, NULL);
+// 	if (child_process_is_running(false, true))
+// 	{
+// 		monitor_sigquit.sa_handler = SIG_DFL;
+// 		monitor_sigquit.sa_flags = SA_RESTART;
+// 		sigemptyset(&monitor_sigquit.sa_mask);
+// 		// sigemptyset(&sig_tobe_blocked);
+// 		// sigaddset(&sig_tobe_blocked, SIGQUIT);
+// 		// if (sigismember(&sig_tobe_blocked, SIGQUIT) == 1)
+// 		// 	sigprocmask(SIG_BLOCK, &sig_tobe_blocked, NULL);
+// 		// rl_on_new_line();
+// 		// if (!is_after_loop(false))
+// 		// 	rl_redisplay();
+// 		// ft_putendl_fd(RED"^\\quit (core dumped) T.T"RESET, STDERR_FILENO);
+// 		// tty_proprieties_manager(true);
+// 	}
+// 	// rl_replace_line("", 0);
+// }
 
-void	quit_signal_unblocker(int sig_nbr)
-{
-	sigset_t	sig_tobe_released;
-
-	tty_proprieties_manager(true);
-	sigemptyset(&sig_tobe_released);
-	sigaddset(&sig_tobe_released, sig_nbr);
-	sigprocmask(SIG_UNBLOCK, &sig_tobe_released, NULL);
-	exit_status_holder(131, true);
-}
-
-void	tty_proprieties_manager(bool update)
+void	tty_proprieties_manager(bool restore)
 {
 	static struct termios	terminal;
 
-	if (update)
+	if (restore)
 		tcsetattr(STDIN_FILENO, TCSANOW, &terminal);
 	else
 		tcgetattr(STDIN_FILENO, &terminal);
