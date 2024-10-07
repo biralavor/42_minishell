4f67f551b802f53f0ab4c5f7513387b3cb65aef3
biralavor <umeneses@student.42sp.org.br>
2024-10-06 22:58:56 -0300

    removing invalid free at tilde

--- src/05_expansions/33.expansion_tilde.c
+++ src/05_expansions/33.expansion_tilde.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 16:11:20 by umeneses          #+#    #+#             */
-/*   Updated: 2024/09/17 12:19:52 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/10/06 22:57:40 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,18 +27,25 @@ char	*expansion_tilde_to_home(char *lexeme)
 {
 	char	*home_path;
 	char	*new_lexeme;
+	bool 	tilde_not_alone;
 
 	home_path = NULL;
 	new_lexeme = NULL;
+	tilde_not_alone = false;
 	if (lexeme[0] == '~')
 	{
 		if (lexeme[1])
+		{
 			lexeme++;
+			tilde_not_alone = true;
+		}
 		else
 			lexeme[0] = '\0';
 		home_path = lookup_table(env_holder(NULL, false, false), "HOME")->value;
 		new_lexeme = ft_strjoin(home_path, lexeme);
-		free(lexeme);
 	}
+	if (tilde_not_alone)
+		lexeme--;
+	free(lexeme);
 	return (new_lexeme);
 }
