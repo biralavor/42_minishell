d0503ef89ab07d1fc769f9472f6ba61086eb899e
biralavor <umeneses@student.42sp.org.br>
2024-10-08 13:06:38 -0300

    avoiding expansion if the previous lexeme was UNSET

--- src/05_expansions/31.expansion_detectors.c
+++ src/05_expansions/31.expansion_detectors.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   31.expansion_detectors.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tmalheir <tmalheir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 18:31:10 by umeneses          #+#    #+#             */
-/*   Updated: 2024/09/26 14:33:40 by tmalheir         ###   ########.fr       */
+/*   Updated: 2024/10/08 12:37:27 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ bool	expansion_dollar_sign_detector(t_token_list *cmd)
 	while (cmd->lexeme && cmd->lexeme[idx] != '\0')
 	{
 		if (cmd->lexeme[idx] == '$' && cmd->lexeme[idx + 1]
-			&& cmd->lexeme[idx + 1] != ' ')
+			&& cmd->lexeme[idx + 1] != ' '
+			&& ft_strcmp(cmd->prev->lexeme, "unset") != 0)
 			return (true);
 		idx++;
 	}
