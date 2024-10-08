291cc1a3045fad3a37802a3816a37a77c229b185
biralavor <umeneses@student.42sp.org.br>
2024-10-08 17:26:29 -0300

    improving state declaration at state detector

--- src/06_builtins/45.builtin_export_utils.c
+++ src/06_builtins/45.builtin_export_utils.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 13:23:46 by umeneses          #+#    #+#             */
-/*   Updated: 2024/10/08 16:55:09 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/10/08 17:24:05 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,15 +64,20 @@ int	arg_handle_state_detector(int state, char *arg)
 		return (state = 0);
 	else if (env_var_key_rules_at_start(arg[idx]))
 	{
-		while (++idx)
+		if (arg[idx + 1])
 		{
-			if (arg[idx] && env_var_key_rules_at_middle(arg[idx])
-				&& arg[idx + 1] == '=')
-				return (state = 100);
-			else if (arg[idx] == '\0' || (arg[idx]
-				&& env_var_key_rules_at_middle(arg[idx])))
-				return (state = 200);
+			while (++idx && arg[idx] && env_var_key_rules_at_middle(arg[idx]))
+			{
+				if (arg[idx + 1] == '=')
+					return (state = 100);
+				else if (arg[idx + 1] == '\0')
+					return (state = 200);
+				else
+					return (state = 404);
+			}
 		}
+		else
+			return (state = 200);
 	}
 	return (state = 404);
 }
