0c6264992f129613a21f5bb403e47d8a57e75121
Thais Malheiros Assumpcao <tmalheir@c3r3p3.42sp.org.br>
2024-10-08 21:52:08 -0300

    fix in state_200

--- src/02_parser/16.state200_to_state300.c
+++ src/02_parser/16.state200_to_state300.c
@@ -3,19 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   16.state200_to_state300.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tmalheir <tmalheir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 14:37:37 by tmalheir          #+#    #+#             */
-/*   Updated: 2024/09/26 11:05:23 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/10/08 21:50:50 by tmalheir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static bool	is_valid(int type)
+{
+	if (type == WORD || type == SUBSHELL || type == DOUBLE_QUOTES
+		|| type == SINGLE_QUOTES)
+		return (true);
+	return (false);
+}
+
 int	state_200(t_token_list *lst, int syntax_state)
 {
-	if (lst->next && lst->prev && (lst->next->type == WORD
-			|| lst->next->type == SUBSHELL || lst->prev->type == WORD))
+	if (lst->next && lst->prev && (is_valid(lst->next->type) && (is_valid(lst->prev->type))))
 		syntax_state = 200;
 	else
 	{
@@ -24,14 +31,6 @@ int	state_200(t_token_list *lst, int syntax_state)
 		exit_status_holder(2, true);
 	}
 	return (syntax_state);
-}
-
-static bool	is_valid(int type)
-{
-	if (type == WORD || type == SUBSHELL || type == DOUBLE_QUOTES
-		|| type == SINGLE_QUOTES)
-		return (true);
-	return (false);
 }
 
 int	state_300(t_token_list *lst, int syntax_state)
