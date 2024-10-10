f6ffa5cb56434ca1cf746d3ee2ad7ad1c1be8e13
biralavor <umeneses@student.42sp.org.br>
2024-10-10 08:05:49 -0300

    applying norminette

--- src/01.main.c
+++ src/01.main.c
@@ -6,76 +6,27 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 10:53:12 by umeneses          #+#    #+#             */
-/*   Updated: 2024/10/09 20:01:35 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/10/10 08:04:53 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	extra_args(void)
-{
-	ft_printf(RED"Error: minishell does not accept arguments.\n");
-	ft_printf(CYAN"It's a kind of magic, but not that much!\n"RESET);
-	exit (EXIT_FAILURE);
-}
-
-// void	control_d_handler(char *input)
-// {
-// 	if (input == NULL)
-// 	{
-// 		ft_putendl_fd("exit", STDOUT_FILENO);
-// 		// env_holder(NULL, false, true);
-// 		// free(input);
-// 		exit_status_holder(EXIT_SUCCESS, true);
-// 	}
-// }
-
-bool	addto_history_detector(const char *input)
-{
-	int	idx;
-
-	idx = 0;
-	if (!ft_strcmp(input, ""))
-		return (false);
-	else
-	{
-		while (input[idx])
-		{
-			if (input[idx] != 32)
-				return (true);
-			idx++;
-		}
-	}
-	return (false);
-}
-
 int	main(int ac, char **av, char **envp)
 {
 	char	*input;
 
-	if (ac > 1 && av)
-		extra_args();
-	signals_init(is_interactive());
-	tty_proprieties_manager(false);
-	environment_init(envp);
-	// signals_manager(g_sigmonitor);
+	minishell_init(ac, av, envp);
 	while (true)
 	{
 		is_after_loop(false);
 		sigquit_activated();
-		// fprintf(stderr, BLUE"exit_status: %d\n"RESET, exit_status_holder(0, false));
-		// fprintf(stderr, YELLOW"g_sigmonitor na main: %d\n"RESET, g_sigmonitor);
 		input = readline(GREEN"<<< Born Again (mini) SHell >>>$ "RESET);
 		if (input == NULL)
 		{
-			ft_putendl_fd("exit", STDOUT_FILENO);
-			env_holder(NULL, false, true);
-			close(STDIN_FILENO);
-			close(STDOUT_FILENO);
-			close(STDERR_FILENO);
+			control_d_handler(input);
 			return (exit_status_holder(EXIT_SUCCESS, true));
 		}
-		// control_d_handler(input);
 		is_after_loop(true);
 		if (addto_history_detector(input))
 			add_history(input);
---
+++ src/minishell_init.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell_init.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/10 08:02:42 by umeneses          #+#    #+#             */
+/*   Updated: 2024/10/10 08:04:33 by umeneses         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static void	extra_args(void);
+
+void	minishell_init(int ac, char **av, char **envp)
+{
+	if (ac > 1 && av)
+		extra_args();
+	signals_init(is_interactive());
+	tty_proprieties_manager(false);
+	environment_init(envp);
+}
+
+static void	extra_args(void)
+{
+	ft_printf(RED"Error: minishell does not accept arguments.\n");
+	ft_printf(CYAN"It's a kind of magic, but not that much!\n"RESET);
+	exit (EXIT_FAILURE);
+}
+
+void	control_d_handler(char *input)
+{
+	ft_putendl_fd("exit", STDOUT_FILENO);
+	env_holder(NULL, false, true);
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	close(STDERR_FILENO);
+}
+
+bool	addto_history_detector(const char *input)
+{
+	int	idx;
+
+	idx = 0;
+	if (!ft_strcmp(input, ""))
+		return (false);
+	else
+	{
+		while (input[idx])
+		{
+			if (input[idx] != 32)
+				return (true);
+			idx++;
+		}
+	}
+	return (false);
+}
