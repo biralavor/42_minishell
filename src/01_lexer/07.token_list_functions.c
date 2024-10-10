17dc7b455cd5623e9254ede7b0f35b5dedd91115
biralavor <umeneses@student.42sp.org.br>
2024-10-10 17:57:52 -0300

    applying norminette

--- src/01_lexer/07.token_list_functions.c
+++ src/01_lexer/07.token_list_functions.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 10:52:55 by tmalheir          #+#    #+#             */
-/*   Updated: 2024/10/09 11:00:50 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/10/10 17:54:20 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,51 +62,4 @@ void	create_new_node(t_token_list **root, t_token_list *token)
 		curr->next = token;
 		token->prev = curr;
 	}
-}
-
-void	assign_lst_idx(t_token_list *lst)
-{
-	int				idx;
-	t_token_list	*temp;
-
-	idx = 0;
-	temp = lst;
-	while (temp)
-	{
-		temp->idx = idx;
-		idx++;
-		temp = temp->next;
-	}
-}
-
-int	ft_lst_size(t_token_list *lst)
-{
-	int				size;
-	t_token_list	*tmp;
-
-	tmp = lst;
-	size = 0;
-	while (tmp)
-	{
-		size++;
-		tmp = tmp->next;
-	}
-	return (size);
-}
-
-void	free_token_list(t_token_list **lst)
-{
-	t_token_list	*curr;
-	t_token_list	*temp;
-
-	curr = *lst;
-	while (curr)
-	{
-		temp = curr;
-		curr = curr->next;
-		if (temp->lexeme)
-			free(temp->lexeme);
-		free(temp);
-	}
-	*lst = NULL;
 }
