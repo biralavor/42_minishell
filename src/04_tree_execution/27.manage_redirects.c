cdf5965eaf7be7062d40600877a4a0a172101d20
umeneses <umeneses@student.42sp.org.br>
2024-10-11 01:25:09 -0300

    fixing merge

--- src/04_tree_execution/27.manage_redirects.c
+++ src/04_tree_execution/27.manage_redirects.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 10:20:13 by tmalheir          #+#    #+#             */
-/*   Updated: 2024/10/11 01:22:26 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/10/11 01:24:31 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,6 @@ int	manage_redirect(t_tree *tree, int *flag)
 		return (exit_status_holder(0, false));
 	}
 	if (try_open_redir(tree, &new_fd, flag, std_fd))
-		return (exit_status_holder(0, false));
 		return (exit_status_holder(0, false));
 	finalize_redirect(tree, flag, std_fd);
 	return (exit_status_holder(0, false));
